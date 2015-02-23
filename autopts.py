c450a46e7901fb905152d23010c454a51b7c8623
Ruslan Mstoi <ruslan.mstoi@linux.intel.com>
2015-02-23 10:59:30 +0200

    Added l2cap support

--- autopts.py
+++ autopts.py
@@ -1,15 +1,18 @@
-'''
-PTS automation IronPython script
+'''PTS automation IronPython script
 
 To use it you have to have installed COM interop assembly to the bin directory
 of PTS, like:
 
 cp Interop.PTSConrol.dll C:\Program Files (x86)\Bluetooth SIG\Bluetooth PTS\bin\
 
-Since PTS requires admin rights, you have to run this script as admin.
+Since PTS requires admin rights, you have to run this script as admin. You need
+to use 32 bit IronPython to run this script because PTS is a 32 bit
+application.
 
-You need to use 32 bit IronPython to run this script because PTS is a 32 bit
-application
+Run this is script in admin terminal as follows:
+
+ipy.exe autopts.py
+
 '''
 
 import clr
@@ -28,7 +31,9 @@ clr.AddReferenceToFileAndPath(
 import Interop.PTSControl as p
 
 # WORKSPACE = r'C:\Users\rmstoi\Documents\Profile Tuning Suite\AOSP on Mako\AOSP on Mako.pqw6'
-WORKSPACE = r'C:\Users\rmstoi\Documents\Profile Tuning Suite\AOSP on HammerHead\AOSP on HammerHead.pqw6'
+# WORKSPACE = r'C:\Users\rmstoi\Documents\Profile Tuning Suite\AOSP on HammerHead\AOSP on HammerHead.pqw6'
+
+WORKSPACE = r'C:\Users\rmstoi\Documents\Profile Tuning Suite\AOSP on Flo\AOSP on Flo.pqw6'
 
 # TODO: adb root should be executed beforehand, otherwise none of the commands will work on device
 ADB = r"C:\Users\rmstoi\AppData\Local\Android\android-sdk\platform-tools\adb.exe"
@@ -112,7 +117,7 @@ class Logger(p.IPTSControlClientLogger):
 class Sender(p.IPTSImplicitSendCallbackEx):
     def OnImplicitSend(self, project_name, wid, test_case, description, style,
                        response, response_size, response_is_present):
-        print "********************"
+        print "\n********************"
         print "BEGIN OnImplicitSend:"
         print "project_name:", project_name
         print "wid:", wid
@@ -124,7 +129,15 @@ class Sender(p.IPTSImplicitSendCallbackEx):
         print "response_is_present:", response_is_present, type(response_is_present)
 
         response_is_present.Value = 1
-        libc.wcscpy_s(response, response_size, u"OK")
+
+        # answer yes to yes/no question, like in case of
+        # if wid == 22 and project_name == "L2CAP" and test_case == "TC_COS_CFC_BV_01_C":
+        if style == 0x11044:
+            libc.wcscpy_s(response, response_size, u"Yes")
+
+        # actually style == 0x11141
+        else:
+            libc.wcscpy_s(response, response_size, u"OK")
 
         print "written resonse is:"
         libc._putws(response)
@@ -141,6 +154,41 @@ class Sender(p.IPTSImplicitSendCallbackEx):
         if wid == 15 and project_name == "RFCOMM" and test_case == "TC_RFC_BV_04_C":
             stop_child = True
         if wid == 14 and project_name == "RFCOMM" and test_case == "TC_RFC_BV_07_C":
+            stop_child = True
+
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CED_BV_01_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CED_BV_03_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CED_BV_04_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CED_BV_09_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CFD_BV_02_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CFD_BV_03_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CFD_BV_08_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CFD_BV_09_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CFD_BV_11_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_COS_CFD_BV_12_C":
+            stop_child = True
+        if wid == 22 and project_name == "L2CAP" and test_case == "TC_COS_IEX_BV_01_C":
+            stop_child = True
+        if wid == 22 and project_name == "L2CAP" and test_case == "TC_COS_CFC_BV_01_C":
+            stop_child = True
+        if wid == 22 and project_name == "L2CAP" and test_case == "TC_COS_CFC_BV_02_C":
+            stop_child = True
+        if wid == 22 and project_name == "L2CAP" and test_case == "TC_COS_CFC_BV_03_C":
+            stop_child = True
+        if wid == 22 and project_name == "L2CAP" and test_case == "TC_COS_CFC_BV_04_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_CMC_BV_10_C":
+            stop_child = True
+        if wid == 14 and project_name == "L2CAP" and test_case == "TC_CMC_BV_11_C":
             stop_child = True
 
         if stop_child and CHILD_PROCESS:
@@ -170,6 +218,8 @@ def run_test_case(project, test_case, command = None):
 
     PTS.RunTestCase(project, test_case)
 
+    # TODO: pointless to sleep with last test case
+
     # in accordance with PTSControlClient.cpp:
     # // Allow device to settle down
     # Sleep(3000);
@@ -181,6 +231,174 @@ def run_test_case(project, test_case, command = None):
 
     CHILD_PROCESS = None
     CHILD_PROCESS_COMMAND = None
+
+def test_l2cap():
+    run_test_case("L2CAP", "TC_COS_CED_BV_01_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CED_BV_03_C", "%s shell l2test -y -N 1 -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CED_BV_04_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+
+    # TODO: PTS issue #12351
+    # the command is
+    # btmgmt power off && btmgmt ssp off && btmgmt power on && l2test -r -P 4113 00:1B:DC:07:32:03 && btmgmt ssp on
+    #
+    # for some reason without power off I get
+    # "Set Secure Simple Pairing for hci0 failed with status 0x0a (Busy)"
+    # so problem with that command also ssp on won't run before l2test is killed, which
+    # will kill whole command
+    #
+    # Hence, support for multiple commands is needed
+    # run_test_case("L2CAP", "TC_COS_CED_BV_05_C", "%s shell btmgmt ssp off;l2test -r -P 4113 %s; btmgmt ssp on" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CED_BV_07_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CED_BV_08_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CED_BV_09_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CED_BV_11_C", "%s shell l2test -u -P 4113 %s" % (ADB, BD_ADDR))
+
+    run_test_case("L2CAP", "TC_COS_CED_BI_01_C")
+
+    # TODO: just like TC_COS_CED_BV_05_C
+    # run_test_case("L2CAP", "TC_COS_CFD_BV_01_C")
+    run_test_case("L2CAP", "TC_COS_CFD_BV_02_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CFD_BV_03_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CFD_BV_08_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CFD_BV_09_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CFD_BV_11_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CFD_BV_12_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+
+    run_test_case("L2CAP", "TC_COS_IEX_BV_01_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_IEX_BV_02_C")
+
+    run_test_case("L2CAP", "TC_COS_ECH_BV_01_C")
+    run_test_case("L2CAP", "TC_COS_ECH_BV_02_C", "%s shell l2ping -c1 %s" % (ADB, BD_ADDR))
+
+    # TODO: the following three cases require advertising to be off
+    # todo this one gives Unknown L2CA CM message, even in pts
+    # run_test_case("L2CAP", "TC_COS_CFC_BV_01_C", "%s shell l2test -y -N 1 -b 40 -V le_public -P 37 %s" % (ADB, BD_ADDR))
+    # run_test_case("L2CAP", "TC_COS_CFC_BV_02_C", "%s shell l2test -y -N 1 -b 1 -V le_public -P 37 %s" % (ADB, BD_ADDR))
+    # TODO: this one gets huge amount of messages, unlike unlike ui and does not pass
+    # run_test_case("L2CAP", "TC_COS_CFC_BV_03_C", "%s shell l2test -u -V le_public -P 37 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_COS_CFC_BV_04_C", "%s shell l2test -u -V le_public -P 37 %s" % (ADB, BD_ADDR))
+    # TODO: this one requiers two l2test processes
+    # run_test_case("L2CAP", "TC_COS_CFC_BV_05_C", "%s shell l2test -u -V le_public -P 37 %s" % (ADB, BD_ADDR))
+
+    run_test_case("L2CAP", "TC_CLS_UCD_BV_01_C")
+    run_test_case("L2CAP", "TC_CLS_UCD_BV_02_C", "%s shell l2test -s -G -N 1 -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_CLS_UCD_BV_03_C", "%s shell l2test -s -E -G -N 1 -P 4113 %s" % (ADB, BD_ADDR))
+
+    run_test_case("L2CAP", "TC_EXF_BV_01_C")
+    run_test_case("L2CAP", "TC_EXF_BV_02_C")
+    run_test_case("L2CAP", "TC_EXF_BV_03_C")
+    run_test_case("L2CAP", "TC_EXF_BV_05_C")
+
+    run_test_case("L2CAP", "TC_CMC_BV_01_C", "%s shell l2test -r -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BV_02_C", "%s shell l2test -r -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BV_03_C", "%s shell l2test -r -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BV_04_C", "%s shell l2test -r -X streaming -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BV_05_C", "%s shell l2test -r -X streaming -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BV_06_C", "%s shell l2test -r -X streaming -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BV_07_C", "%s shell l2test -r -X ertm -P 4113" % (ADB,))
+    # TODO: these two seem to be reluctant to pass, with UI removing link key solves it
+    run_test_case("L2CAP", "TC_CMC_BV_08_C", "%s shell l2test -r -X streaming -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BV_09_C", "%s shell l2test -r -X basic -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BV_10_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_CMC_BV_11_C", "%s shell l2test -n -P 4113 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_CMC_BV_12_C",  "%s shell l2test -z -X ertm %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_CMC_BV_13_C",  "%s shell l2test -z -X streaming %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_CMC_BV_14_C",  "%s shell l2test -r -X streaming -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BV_15_C",  "%s shell l2test -r -X streaming -P 4113" % (ADB,))
+
+    # TODO: seem to get INCONC, need to remove link key?
+    run_test_case("L2CAP", "TC_CMC_BI_01_C",  "%s shell l2test -r -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BI_02_C",  "%s shell l2test -r -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BI_03_C",  "%s shell l2test -r -X streaming -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BI_04_C",  "%s shell l2test -r -X streaming -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BI_05_C",  "%s shell l2test -r -X basic -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_CMC_BI_06_C",  "%s shell l2test -r -X basic -P 4113" % (ADB,))
+
+    # TODO: inconc
+    run_test_case("L2CAP", "TC_FOC_BV_01_C",  "%s shell l2test -r -X ertm -P 4113 -F 0" % (ADB,))
+    run_test_case("L2CAP", "TC_FOC_BV_02_C",  "%s shell l2test -r -X ertm -P 4113 -F 0" % (ADB,))
+    run_test_case("L2CAP", "TC_FOC_BV_03_C",  "%s shell l2test -r -X ertm -P 4113 -F 0" % (ADB,))
+
+    # TODO: inconc
+    run_test_case("L2CAP", "TC_OFS_BV_01_C",  "%s shell l2test -x -X ertm -P 4113 -F 0 -N 1" % (ADB,))
+    run_test_case("L2CAP", "TC_OFS_BV_02_C",  "%s shell l2test -r -X ertm -P 4113 -F 0" % (ADB,))
+    run_test_case("L2CAP", "TC_OFS_BV_03_C",  "%s shell l2test -x -X streaming -P 4113 -F 0 -N 1" % (ADB,))
+    run_test_case("L2CAP", "TC_OFS_BV_04_C",  "%s shell l2test -d -X streaming -P 4113 -F 0" % (ADB,))
+    run_test_case("L2CAP", "TC_OFS_BV_05_C",  "%s shell l2test -x -X ertm -P 4113 -N 1" % (ADB,))
+    run_test_case("L2CAP", "TC_OFS_BV_06_C",  "%s shell l2test -r -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_OFS_BV_07_C",  "%s shell l2test -x -X streaming -P 4113 -F 0 -N 1" % (ADB,))
+    run_test_case("L2CAP", "TC_OFS_BV_08_C",  "%s shell l2test -d -X streaming -P 4113" % (ADB,))
+
+    run_test_case("L2CAP", "TC_ERM_BV_01_C",  "%s shell l2test -x -X ertm -P 4113 -N 3 -Y 3" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_02_C",  "%s shell l2test -r -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_03_C",  "%s shell l2test -r -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_05_C",  "%s shell l2test -x -X ertm -P 4113 -N 2 -Y 2" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_06_C",  "%s shell l2test -x -X ertm -P 4113 -N 2 -Y 2" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_07_C",  "%s shell l2test -r -H 1000 -K 10000 -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_08_C",  "%s shell l2test -x -X ertm -P 4113 -N 1" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_09_C",  "%s shell l2test -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_10_C",  "%s shell l2test -x -X ertm -P 4113 -N 1" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_11_C",  "%s shell l2test -x -X ertm -P 4113 -N 1 -Q 1" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_12_C",  "%s shell l2test -x -X ertm -P 4113 -R -N 1 -Q 1" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_13_C",  "%s shell l2test -x -X ertm -P 4113 -N 2" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_14_C",  "%s shell l2test -x -X ertm -P 4113 -N 4" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_15_C",  "%s shell l2test -x -X ertm -P 4113 -N 4" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_17_C",  "%s shell l2test -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_18_C",  "%s shell l2test -x -X ertm -P 4113 -N 1" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_19_C",  "%s shell l2test -x -X ertm -P 4113 -N 1" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_20_C",  "%s shell l2test -x -X ertm -P 4113 -N 1" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_21_C",  "%s shell l2test -x -X ertm -P 4113 -D 2000 -N 2" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_22_C",  "%s shell l2test -r -H 1000 -K 10000 -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BV_23_C",  "%s shell l2test -x -X ertm -P 4113 -N 2" % (ADB,))
+
+    run_test_case("L2CAP", "TC_ERM_BI_02_C",  "%s shell l2test -X ertm -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BI_03_C",  "%s shell l2test -x -X ertm -P 4113 -N 2" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BI_04_C",  "%s shell l2test -x -X ertm -P 4113 -N 2" % (ADB,))
+    run_test_case("L2CAP", "TC_ERM_BI_05_C",  "%s shell l2test -x -X ertm -P 4113 -N 2" % (ADB,))
+
+    run_test_case("L2CAP", "TC_STM_BV_01_C",  "%s shell l2test -x -X streaming -P 4113 -N 3 -Y 3" % (ADB,))
+    run_test_case("L2CAP", "TC_STM_BV_02_C",  "%s shell l2test -d -X streaming -P 4113" % (ADB,))
+    run_test_case("L2CAP", "TC_STM_BV_03_C",  "%s shell l2test -x -X streaming -P 4113 -N 2" % (ADB,))
+
+    run_test_case("L2CAP", "TC_FIX_BV_01_C",  "%s shell l2test -z -P 4113 %s" % (ADB, BD_ADDR))
+
+    run_test_case("L2CAP", "TC_LE_CPU_BV_01_C",  "%s shell l2test -n -V le_public -J 4" % (ADB,))
+    run_test_case("L2CAP", "TC_LE_CPU_BV_02_C",  "%s shell l2test -n -V le_public -J 4 %s" % (ADB, BD_ADDR))
+
+    run_test_case("L2CAP", "TC_LE_CPU_BI_01_C",  "%s shell l2test -n -V le_public -J 4 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_LE_CPU_BI_02_C",  "%s shell l2test -r -V le_public -J 4" % (ADB,))
+
+    run_test_case("L2CAP", "TC_LE_REJ_BI_01_C",  "%s shell l2test -n -V le_public -J 4 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_LE_REJ_BI_02_C",  "%s shell l2test -n -V le_public -J 4 %s" % (ADB, BD_ADDR))
+
+    run_test_case("L2CAP", "TC_LE_CFC_BV_01_C",  "%s shell l2test -n -V le_public -P 37 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_LE_CFC_BV_02_C",  "%s shell l2test -n -V le_public -P 37 %s" % (ADB, BD_ADDR))
+    # Note: PIXIT TSPX_iut_role_initiator=FALSE
+    # run_test_case("L2CAP", "TC_LE_CFC_BV_03_C",  "%s shell l2test -x -N 1 -V le_public %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_LE_CFC_BV_04_C",  "%s shell l2test -n -V le_public -P 241 %s" % (ADB, BD_ADDR))
+
+    # Note: PIXIT TSPX_iut_role_initiator=FALSE
+    # run_test_case("L2CAP", "TC_LE_CFC_BV_05_C",  "%s shell l2test -r -V le_public -J 4" % (ADB,))
+
+    # PTS issue #12853
+    # Note: PIXIT TSPX_iut_role_initiator=FALSE
+    # run_test_case("L2CAP", "TC_LE_CFC_BV_06_C",  "%s shell l2test -x -b 1 -V le_public %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_LE_CFC_BV_07_C",  "%s shell l2test -u -V le_public %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_LE_CFC_BI_01_C",  "%s shell l2test -u -V le_public %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_LE_CFC_BV_08_C",  "%s shell l2test -n -V le_public -P 37 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_LE_CFC_BV_09_C",  "%s shell l2test -n -V le_public -P 37 %s" % (ADB, BD_ADDR))
+    run_test_case("L2CAP", "TC_LE_CFC_BV_16_C",  "%s shell l2test -n -V le_public -P 37 %s" % (ADB, BD_ADDR))
+
+    # PTS issue #12730
+    # l2test -s -N 1 <bdaddr>
+    # l2test -s -N 1 -V le_public <bdaddr>
+    # run_test_case("L2CAP", "TC_LE_CID_BV_02_I",  "%s shell " % (ADB,))
+
+    # PTS issue #12730
+    # Note: PIXIT TSPX_iut_role_initiator=FALSE
+    # l2test -w -N 1
+    # l2test -w -N 1 -V le_public
+    # run_test_case("L2CAP", "TC_LE_CID_BV_01_C",  "%s shell " % (ADB,))
 
 def test_rfcomm():
     run_test_case("RFCOMM", "TC_RFC_BV_01_C", "%s shell rctest -n -P 1 %s" % (ADB, BD_ADDR))
@@ -238,8 +456,9 @@ def main():
     
     print "\n\n\nRunning test cases..."
 
-    test_rfcomm()
-    run_test_case("DID", "TC_SDI_BV_1_I")
+    test_l2cap()
+#    test_rfcomm()
+#    run_test_case("DID", "TC_SDI_BV_1_I")
 
     print_results()
 
