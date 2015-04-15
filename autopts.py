03f08ea1e480080c427486aa01bf7b0499906601
Ruslan Mstoi <ruslan.mstoi@linux.intel.com>
2015-04-15 17:48:41 +0300

    Last gap cases automated
    Test cases using haltest are commented an left for later, because haltest is
    interactive and not easily automated.

--- autopts.py
+++ autopts.py
@@ -225,6 +225,14 @@ class btmgmt:
         exec_iut_cmd("btmgmt connectable off", True)
 
     @staticmethod
+    def bondable_on():
+        exec_iut_cmd("btmgmt bondable on", True)
+
+    @staticmethod
+    def bondable_off():
+        exec_iut_cmd("btmgmt bondable off", True)
+
+    @staticmethod
     def discoverable_on():
         exec_iut_cmd("btmgmt discov on", True)
 
@@ -846,14 +854,14 @@ def get_test_cases_gap():
         TestCase("GAP", "TC_DISC_GENP_BV_02_C", TestCmd("btmgmt find -l")),
         TestCase("GAP", "TC_DISC_GENP_BV_03_C", TestCmd("btmgmt find -l")),
         TestCase("GAP", "TC_DISC_GENP_BV_04_C", TestCmd("btmgmt find -l")),
-        TestCase("GAP", "TC_DISC_GENP_BV_05_C", TestCmd("btmgmt find -l"))
+        TestCase("GAP", "TC_DISC_GENP_BV_05_C", TestCmd("btmgmt find -l")),
 
         TestCase("GAP", "TC_IDLE_GIN_BV_01_C",
                  TestCmd("btmgmt find", start_wid = 146)),
         TestCase("GAP", "TC_IDLE_LIN_BV_01_C",
                  TestCmd("hcitool scan --iac=liac", start_wid = 146)),
 
-        # TODO: how to script haltest
+        # TODO: must script/automate haltest
         # TestCase("GAP", "TC_IDLE_NAMP_BV_01_C"),
 
         TestCase("GAP", "TC_IDLE_NAMP_BV_02_C",
@@ -869,8 +877,104 @@ def get_test_cases_gap():
 
         TestCase("GAP", "TC_CONN_UCON_BV_01_C"),
         TestCase("GAP", "TC_CONN_UCON_BV_02_C"),
-        TestCase("GAP", "TC_CONN_UCON_BV_03_C")
+        TestCase("GAP", "TC_CONN_UCON_BV_03_C"),
+
+        # TODO: must script/automate haltest
+        # TestCase("GAP", "TC_CONN_ACEP_BV_01_C"),
+        # TestCase("GAP", "TC_CONN_GCEP_BV_01_C"),
+        # TestCase("GAP", "TC_CONN_GCEP_BV_02_C"),
+
+        # TestCase("GAP", "TC_CONN_SCEP_BV_01_C"),
+        # TestCase("GAP", "TC_CONN_DCEP_BV_01_C"),
+        # TestCase("GAP", "TC_CONN_DCEP_BV_03_C"),
+        TestCase("GAP", "TC_CONN_CPUP_BV_01_C",
+                 TestFunc(btmgmt.advertising_on)),
+        # TestCase("GAP", "TC_CONN_CPUP_BV_02_C",
+        #          TestFunc(btmgmt.advertising_on)),
+        TestCase("GAP", "TC_CONN_CPUP_BV_03_C",
+                 TestFunc(btmgmt.advertising_on)),
+
+        # TODO: must script/automate haltest
+        # TestCase("GAP", "TC_CONN_CPUP_BV_04_C"),
+        # TestCase("GAP", "TC_CONN_CPUP_BV_05_C"),
+        # TestCase("GAP", "TC_CONN_CPUP_BV_06_C"),
+        # TestCase("GAP", "TC_CONN_TERM_BV_01_C"),
+        # TestCase("GAP", "TC_CONN_PRDA_BV_01_C"),
+        # TestCase("GAP", "TC_CONN_PRDA_BV_02_C"),
+        # TestCase("GAP", "TC_BOND_NBON_BV_01_C"),
+        # TestCase("GAP", "TC_BOND_NBON_BV_02_C"),
+        # TestCase("GAP", "TC_BOND_NBON_BV_03_C"),
+        # TestCase("GAP", "TC_BOND_BON_BV_01_C"),
+        # TestCase("GAP", "TC_BOND_BON_BV_02_C"),
+        # TestCase("GAP", "TC_BOND_BON_BV_03_C"),
+        # TestCase("GAP", "TC_BOND_BON_BV_04_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_11_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_12_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_13_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_14_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_17_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_18_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_19_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_20_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_21_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_22_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_23_C"),
+        # TestCase("GAP", "TC_SEC_AUT_BV_24_C"),
+        # TestCase("GAP", "TC_SEC_CSIGN_BV_01"),
+        # TestCase("GAP", "TC_SEC_CSIGN_BV_02"),
+        # TestCase("GAP", "TC_SEC_CSIGN_BI_01_C"),
+        # TestCase("GAP", "TC_SEC_CSIGN_BI_02_C"),
+        # TestCase("GAP", "TC_SEC_CSIGN_BI_03_C"),
+        # TestCase("GAP", "TC_SEC_CSIGN_BI_04_C"),
+        #
+        # PTS issue #12951
+        # Note: PIXITs required to be changed:
+        # TSPX_using_public_device_address: FALSE
+        # TSPX_using_random_device_address: TRUE
+        # echo 30 > /sys/kernel/debug/bluetooth/hci0/
+        # 				rpa_timeout
+        # btmgmt power off
+        # btmgmt privacy on
+        # btmgmt power on
+        # TestCase("GAP", "TC_PRIV_CONN_BV_10_C")
+        #
+        # INC
+        # PTS issue #12952
+        # JIRA #BA-186
+        # TestCase("GAP", "TC_PRIV_CONN_BV_11_C")
+        # TODO: must script/automate haltest
+        # TestCase("GAP", "TC_ADV_BV_02_C"),
+        # TestCase("GAP", "TC_ADV_BV_03_C"),
+        # TestCase("GAP", "TC_ADV_BV_05_C"),
+        # TestCase("GAP", "TC_GAT_BV_01_C"),
+        # TestCase("GAP", "TC_DM_NCON_BV_01_C"),
+        # TestCase("GAP", "TC_DM_CON_BV_01_C"),
+        # TestCase("GAP", "TC_DM_NBON_BV_01_C", TestFunc(btmgmt.bondable_off)),
+        # TODO: must script/automate haltest
+        # TestCase("TC_DM_BON_BV_01_C"),
+        TestCase("GAP", "TC_DM_GIN_BV_01_C"),
+        TestCase("GAP", "TC_DM_LIN_BV_01_C"),
+        TestCase("GAP", "TC_DM_NAD_BV_01_C"),
+        # TestCase("GAP", "TC_DM_NAD_BV_02_C"),
+
+        # TODO: must script/automate haltest
+        # TestCase("GAP", "TC_DM_LEP_BV_01_C"),
+        # TestCase("GAP", "TC_DM_LEP_BV_02_C"),
+        # TestCase("GAP", "TC_DM_LEP_BV_04_C",
+        #          TestCmd("l2test -n %s" % PTS_BD_ADDR)),
+        # TestCase("GAP", "TC_DM_LEP_BV_05_C",
+        #          [TestCmd("btmgmt find -b"),
+        #           TestCmd("l2test -n %s" % PTS_BD_ADDR)])
+
+        # TODO: must script/automate haltest
+        # TestCase("GAP", "TC_DM_LEP_BV_06_C"),
+        # TestCase("GAP", "TC_DM_LEP_BV_07_C"),
+        # TestCase("GAP", "TC_DM_LEP_BV_08_C"),
+        # TestCase("GAP", "TC_DM_LEP_BV_09_C"),
+        # TestCase("GAP", "TC_DM_LEP_BV_10_C"),
+        # TestCase("GAP", "TC_DM_LEP_BV_11_C"),
     ]
+
     return test_cases
 
 def parse_args():
