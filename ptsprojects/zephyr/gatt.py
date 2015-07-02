963e4e811806ed67cdcf8b0014384f7220b24c2c
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@tieto.com>
2015-07-02 15:42:26 +0200

    Introduce initial Zephyr gatt test cases stub
    This patch introduces initial Zephyr gatt test cases file definition
    with stubs.

---
+++ ptsprojects/zephyr/gatt.py
@@ -0,0 +1,46 @@
+"""GATT test cases"""
+
+try:
+    from ptsprojects.testcase import TestCase, TestCmd, TestFunc, \
+        TestFuncCleanUp
+    from ptsprojects.zephyr.qtestcase import QTestCase
+
+except ImportError: # running this module as script
+    import sys
+    sys.path.append("../..") # to be able to locate the following imports
+
+    from ptsprojects.testcase import TestCase, TestCmd, TestFunc, \
+        TestFuncCleanUp
+    from ptsprojects.zephyr.qtestcase import QTestCase
+
+from ptsprojects.zephyr.iutctl import get_zephyr
+
+def test_cases():
+    """Returns a list of GAP test cases
+    pts -- Instance of PyPTS"""
+
+    zephyrctl = get_zephyr()
+
+    test_cases = [
+    ]
+
+    return test_cases
+
+def main():
+    """Main."""
+    import sys
+    import ptsprojects.zephyr.iutctl as iutctl
+
+    # to be able to successfully create ZephyrCtl in QTestCase
+    iutctl.ZEPHYR_KERNEL_IMAGE = sys.argv[0]
+
+    test_cases_ = test_cases()
+
+    for test_case in test_cases_:
+        print
+        print test_case
+        for index, cmd in enumerate(test_case.cmds):
+            print "%d) %s" % (index, cmd)
+
+if __name__ == "__main__":
+    main()
