3c51895d1109d77e6c284ccc974455ea24f9243d
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@tieto.com>
2015-09-08 12:44:01 +0200

    zephyr: Update btp message definition
    Major update message definition file to new btp version. This patch
    also keep up to date value of btp frame header length.

--- ptsprojects/zephyr/msgdefs.py
+++ ptsprojects/zephyr/msgdefs.py
@@ -1,18 +1,62 @@
 '''BTP header message definition'''
 
-HDR_LEN = 4
+HDR_LEN = 5
+
+CONTROLLER_INDEX = '\x00'
+BTP_INDEX_NONE = '\xff'
 
 #Service ID dictionary
-SERVICE_ID = dict(SERVICE_ID_CORE = '\x00', SERVICE_ID_GAP = '\x01')
+SERVICE_ID = dict(
+                  SERVICE_ID_CORE = '\x00',
+                  SERVICE_ID_GAP = '\x01',
+                  )
 
 #Core Service
-CORE_SERVICE_OP = dict(OP_STATUS = '\x00', OP_REGISTER_SERVICE = '\x01')
+# COMMANDS
+CORE_SERVICE_OP = dict(
+                       CORE_READ_SUPPORTED_COMMANDS = '\x01',
+                       CORE_READ_SUPPORTED_SERVICES = '\x02',
+                       CORE_REGISTER_SERVICE = '\x03',
+                       )
 
 #GAP Service
-GAP_SERVICE_OP = dict(OP_STATUS = '\x00', OP_GAP_START_ADV = '\x01')
+# COMMANDS
+GAP_SERVICE_OP = dict(
+                      GAP_READ_SUPPORTED_COMMANDS = '\x01',
+                      GAP_READ_CONTROLLER_INDEX_LIST = '\x02',
+                      GAP_READ_CONTROLLER_INFO = '\x03',
+                      GAP_RESET = '\x04',
+                      GAP_SET_POWERED = '\x05',
+                      GAP_SET_CONNECTABLE = '\x06',
+                      GAP_SET_FAST_CONNECTABLE = '\x07',
+                      GAP_SET_DISCOVERABLE = '\x08',
+                      GAP_SET_BONDABLE = '\x09',
+                      GAP_START_ADVERTISING = '\x0a',
+                      GAP_STOP_ADVERTISING = '\x0b',
+                      )
 
-ADV_TYPE = dict(ADV_IND = '\x00', ADV_DIRECT_IND_HD = '\x01', ADV_SCAN_IND = '\x02',
-                ADV_NONCONN_IND = '\x03', ADV_DIRECT_IND_LD = '\x04')
+ADV_TYPE = dict(
+                ADV_IND = '\x00',
+                ADV_DIRECT_IND_HD = '\x01',
+                ADV_SCAN_IND = '\x02',
+                ADV_NONCONN_IND = '\x03',
+                ADV_DIRECT_IND_LD = '\x04',
+                )
+
+# EVENTS
+GAP_SERVICE_EV = dict(
+                      GAP_EV_NEW_SETTINGS = '\x80',
+                      GAP_EV_DEVICE_FOUND = '\x81',
+                      GAP_EV_DEVICE_CONNECTED = '\x82',
+                      GAP_EV_DEVICE_DISCONNECTED = '\x83',
+                      )
+
 #Status
-STATUS = dict(STATUS_SUCCESS = '\x00', STATUS_FAILED = '\x01', STATUS_UNKNOWN_CMD = '\x02',
-              STATUS_NOT_READY = '\x03')
+BTP_STATUS = '\x00'
+
+STATUS = dict(
+              STATUS_SUCCESS = '\x00',
+              STATUS_FAILED = '\x01',
+              STATUS_UNKNOWN_CMD = '\x02',
+              STATUS_NOT_READY = '\x03',
+              )
