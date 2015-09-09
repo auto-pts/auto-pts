3aed5168f006617b8eb1ff251dd55f8f88adcc30
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@tieto.com>
2015-09-09 16:26:14 +0200

    zephyr: Update btp message wrapers to new btp version
    Possible messages should be updated with new btp version.

--- ptsprojects/zephyr/btp.py
+++ ptsprojects/zephyr/btp.py
@@ -1,28 +1,42 @@
 """Wrapper around btp messages. The functions are added as needed."""
 
 from iutctl import get_zephyr
-from msgdefs import SERVICE_ID, CORE_SERVICE_OP, GAP_SERVICE_OP, ADV_TYPE
+from msgdefs import SERVICE_ID, CORE_SERVICE_OP, GAP_SERVICE_OP, BTP_INDEX_NONE, CONTROLLER_INDEX
+
+CORE = {
+    "gap_reg": (SERVICE_ID['SERVICE_ID_CORE'], CORE_SERVICE_OP['CORE_REGISTER_SERVICE'],
+        BTP_INDEX_NONE, (SERVICE_ID['SERVICE_ID_GAP'],)),
+}
+
+GAP = {
+    "read_supp_cmds": '',
+    "read_ctrl_index_list": '',
+    "read_ctrl_info": '',
+    "reset": '',
+    "set_powered": '',
+    "set_connectable": '',
+    "set_fast_connectable": '',
+    "set_discov": '',
+    "set_bond": '',
+    "start_adv": (SERVICE_ID['SERVICE_ID_GAP'], GAP_SERVICE_OP['GAP_START_ADVERTISING'],
+        CONTROLLER_INDEX, ()),
+    "stop_adv": '',
+    "start_discov": '',
+    "stop_discov": '',
+}
 
 def gap_reg_svc():
     zephyrctl = get_zephyr()
 
-    zephyrctl.sock_send(SERVICE_ID['SERVICE_ID_CORE'], CORE_SERVICE_OP['OP_REGISTER_SERVICE'],
-                     (SERVICE_ID['SERVICE_ID_GAP'],))
+    zephyrctl.sock_send(*CORE['gap_reg'])
 
 def gap_adv_ind_on():
     zephyrctl = get_zephyr()
 
-    zephyrctl.sock_send(SERVICE_ID['SERVICE_ID_GAP'], GAP_SERVICE_OP['OP_GAP_START_ADV'],
-                     (ADV_TYPE['ADV_IND'],))
-
-def gap_adv_ind_nonconn_on():
-    zephyrctl = get_zephyr()
-
-    zephyrctl.sock_send(SERVICE_ID['SERVICE_ID_GAP'], GAP_SERVICE_OP['OP_GAP_START_ADV'],
-                     (ADV_TYPE['ADV_NONCONN_IND'],))
+    zephyrctl.sock_send(*GAP['start_adv'])
 
 def gap_reg_svc_rsp_succ():
     zephyrctl = get_zephyr()
 
-    zephyrctl.sock_read(SERVICE_ID['SERVICE_ID_CORE'], CORE_SERVICE_OP['OP_REGISTER_SERVICE'],
-                     None)
+    zephyrctl.sock_read(SERVICE_ID['SERVICE_ID_CORE'], CORE_SERVICE_OP['CORE_REGISTER_SERVICE'],
+                        BTP_INDEX_NONE, None)
