c5dcd2d389d814b492d2214ad726a1d6bbd71a8e
Ruslan Mstoi <ruslan.mstoi@linux.intel.com>
2015-09-24 11:55:20 +0300

    Fixed indentation and backslashed multiline string

--- ptsprojects/zephyr/iutctl.py
+++ ptsprojects/zephyr/iutctl.py
@@ -40,10 +40,10 @@ class ZephyrCtl:
 
         log("%s.%s", self.__class__, self.start.__name__)
 
-        qemu_cmd = "%s -cpu cortex-m3 -machine lm3s6965evb -nographic " \
-                        "-serial mon:stdio -serial unix:/tmp/bt-server-bredr " \
-                        "-serial unix:%s -kernel %s" \
-                        % (QEMU_BIN, QEMU_UNIX_PATH, self.kernel_image)
+        qemu_cmd = ("%s -cpu cortex-m3 -machine lm3s6965evb -nographic "
+                    "-serial mon:stdio -serial unix:/tmp/bt-server-bredr "
+                    "-serial unix:%s -kernel %s" %
+                    (QEMU_BIN, QEMU_UNIX_PATH, self.kernel_image))
 
         log("Starting QEMU zephyr process: %s", qemu_cmd)
 
