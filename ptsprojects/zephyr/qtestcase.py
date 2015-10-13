582fb9b356b9d601156b8bfc7961f75c16bf65f3
Ruslan Mstoi <ruslan.mstoi@linux.intel.com>
2015-10-13 14:34:13 +0300

    Fixed indentation in QTestCase

--- ptsprojects/zephyr/qtestcase.py
+++ ptsprojects/zephyr/qtestcase.py
@@ -5,23 +5,23 @@ from ptsprojects.testcase import TestCase, TestFunc, \
 from ptsprojects.zephyr.iutctl import get_zephyr
 
 class QTestCase(TestCase):
-        """A test case that uses QEMU as DUT"""
+    """A test case that uses QEMU as DUT"""
 
-        def __init__(self, project_name, test_case_name, cmds = [], no_wid = None):
-            """cmds - a list of TestCmd and TestFunc or single instance of them
-            no_wid - a wid (tag) to respond No to"""
+    def __init__(self, project_name, test_case_name, cmds = [], no_wid = None):
+        """cmds - a list of TestCmd and TestFunc or single instance of them
+        no_wid - a wid (tag) to respond No to"""
 
-            super(QTestCase, self).__init__(project_name,
-                                            test_case_name,
-                                            cmds,
-                                            no_wid)
+        super(QTestCase, self).__init__(project_name,
+                                        test_case_name,
+                                        cmds,
+                                        no_wid)
 
-            self.zephyrctl = get_zephyr()
+        self.zephyrctl = get_zephyr()
 
-            # first command is to start QEMU
-            self.cmds.insert(0, TestFunc(self.zephyrctl.socks_open))
-            self.cmds.insert(1, TestFunc(self.zephyrctl.start))
-            self.cmds.insert(2, TestFunc(self.zephyrctl.socks_accept))
+        # first command is to start QEMU
+        self.cmds.insert(0, TestFunc(self.zephyrctl.socks_open))
+        self.cmds.insert(1, TestFunc(self.zephyrctl.start))
+        self.cmds.insert(2, TestFunc(self.zephyrctl.socks_accept))
 
-            # last command is to stop QEMU
-            self.cmds.append(TestFuncCleanUp(self.zephyrctl.stop))
+        # last command is to stop QEMU
+        self.cmds.append(TestFuncCleanUp(self.zephyrctl.stop))
