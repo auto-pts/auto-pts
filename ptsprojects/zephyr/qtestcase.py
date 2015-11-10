3325b21a8497b9362efb31306462368b98f0fdd3
Ruslan Mstoi <ruslan.mstoi@linux.intel.com>
2015-11-10 17:13:49 +0200

    Change QTestCase.__init__ to use arbitrary argument lists
    This way any new arguments or changes in the argument list of the parent
    TestCase.__init__ does not need to be done also in QTestCase

--- ptsprojects/zephyr/qtestcase.py
+++ ptsprojects/zephyr/qtestcase.py
@@ -7,29 +7,10 @@ from ptsprojects.zephyr.iutctl import get_zephyr
 class QTestCase(TestCase):
     """A test case that uses QEMU as DUT"""
 
-    def __init__(self, project_name, test_case_name, cmds = [], no_wid = None,
-                 edit1_wids = None, verify_wids = None):
-        """
-        cmds -- a list of TestCmd and TestFunc or single instance of them
+    def __init__(self, *args, **kwargs):
+        """Refer to TestCase.__init__ for parameters and their documentation"""
 
-        no_wid -- a wid (tag) to respond No to
-
-        edit1_wids -- A dictionary of wids as keys and string input as values.
-                      The value is send to PTS in response to MMI_Style_Edit1
-                      style prompts with matching wid.
-
-        verify_wids -- A dictionary of wids as keys and a tuple of strings as
-                       values. The strings are used with MMI_Style_Yes_No1 to
-                       confirm/verify that the MMI description contains all of
-                       the strings in the tuple.
-        """
-
-        super(QTestCase, self).__init__(project_name,
-                                        test_case_name,
-                                        cmds,
-                                        no_wid,
-                                        edit1_wids,
-                                        verify_wids)
+        super(QTestCase, self).__init__(*args, **kwargs)
 
         self.zephyrctl = get_zephyr()
 
