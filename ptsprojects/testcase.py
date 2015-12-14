ce7e891ae79a1fbe6fd3bd046a28dbbb9b695bc5
Ruslan Mstoi <ruslan.mstoi@linux.intel.com>
2015-12-14 17:11:24 +0200

    Added message to assertion of test case name and project name
    This way it will be easier to find what went wrong from logs

--- ptsprojects/testcase.py
+++ ptsprojects/testcase.py
@@ -444,8 +444,13 @@ class TestCase(PTSCallback):
         log("%s %s", self, self.on_implicit_send.__name__)
 
         # this should never happen, pts does not run tests in parallel
-        assert project_name == self.project_name and \
-            test_case_name == self.name
+        assert project_name == self.project_name, \
+            "Unexpected project name %r should be %r" % \
+            (project_name, self.project_name)
+
+        assert test_case_name == self.name, \
+            "Unexpected test case name %r should be %r" % \
+            (test_case_name, self.name)
 
         my_response = ""
 
