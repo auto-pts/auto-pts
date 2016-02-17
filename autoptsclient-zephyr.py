d22bd80e9d97537539a9f5488a874f89e7d216e4
Mariusz Skamra <mariusz.skamra@tieto.com>
2016-02-17 15:31:43 +0100

    zephyr: Set PIXIT parameters for the GATT test cases
    This adds a function to be called to update appropriate PIXIT
    parameters for the test cases.
    It's helpful to have this, because PIXIT are set to default everytime
    the TPG is updated. To avoid starting PTS in gui mode to update the
    pixits, this can be done using this function. Then we're sure that
    those are set to appropriate value.

--- autoptsclient-zephyr.py
+++ autoptsclient-zephyr.py
@@ -44,6 +44,18 @@ def parse_args():
 
     return args
 
+def update_pixit(proxy):
+    """Function used to update PIXIT parameters.
+
+    PIXITs are set to default everytime the TPG is updated. To avoid starting
+    PTS in gui mode to update the PIXITs, this can be done using this function.
+    Then we're sure that those are set to appropriate value.
+    """
+    proxy.update_pixit_param("GATT", "TSPX_delete_link_key", "TRUE")
+    proxy.update_pixit_param("GATT", "TSPX_delete_ltk", "TRUE")
+
+    return
+
 def main():
     """Main."""
     if os.geteuid() == 0: # root privileges are not needed
@@ -59,6 +71,8 @@ def main():
     # in some networks initial connection setup is very slow, so, contact the
     # server only once to get data needed to create test cases
     pts_bd_addr = proxy.bd_addr()
+
+    update_pixit(proxy)
 
     test_cases = autoprojects.gap.test_cases(pts_bd_addr)
     test_cases += autoprojects.gatt.test_cases(pts_bd_addr)
