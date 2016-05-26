auto-pts/auto-pts

commit 8c20d920e816f481ff555824bb99824ff7d6b82e

nble: Workaround TC_GAN_SR_BV_01_C test case
Test only passes when there are two characterisitcs with Notify
property in the database.
Note that PTS is enabling notifications from the first Characteristic
and asking to send notifications from handle of the second in MMI.
When there is only one characteristic that can be notified test fails.

ptsprojects/nble/gatt.py
@@ -19,6 +19,7 @@ except ImportError:  # running this module as script
 
 from ptsprojects.zephyr.iutctl import get_zephyr
 import ptsprojects.zephyr.btp as btp
+import time
 
 
 class UUID:
@@ -151,7 +152,7 @@ class Perm:
         return decode_flag_name(perm, Perm.names)
 
 
-def test_cases_server():
+def test_cases_server(pts_bd_addr):
     """Returns a list of GATT Server test cases"""
 
     zephyrctl = get_zephyr()
@@ -1093,14 +1094,22 @@ def test_cases_server():
                    TestFunc(btp.core_reg_svc_gatts),
                    TestFunc(btp.gatts_add_svc, 0, UUID.VND16_1),
                    TestFunc(btp.gatts_add_char, 0,
-                            Prop.read | Prop.write | Prop.nofity,
-                            Perm.read | Perm.write, UUID.VND16_2),
+                            Prop.nofity | Prop.read, Perm.read, UUID.VND16_2),
                    TestFunc(btp.gatts_set_val, 0, '00'),
                    TestFunc(btp.gatts_add_desc, 0,
                             Perm.read | Perm.write, UUID.CCC),
+
+                   # FIXME Add another characteristic with notify property to
+                   # workaround PTS failure
+                   TestFunc(btp.gatts_add_char, 0,
+                            Prop.nofity | Prop.read, Perm.read, UUID.VND16_3),
+
                    TestFunc(btp.gatts_start_server),
                    TestFunc(btp.gap_adv_ind_on, start_wid=1),
-                   TestFunc(btp.gatts_set_val, 4, '01', start_wid=1)]),
+                   TestFunc(btp.gap_connected_ev, pts_bd_addr,
+                            Addr.le_public, start_wid=92),
+                   TestFunc(time.sleep, 1, start_wid=92),
+                   TestFunc(btp.gatts_set_val, 11, '01', start_wid=92)]),
         # PTS Issue 14646, 14728, RTOS-1523: nble shall return handles
         ZTestCase("GATT", "TC_GAI_SR_BV_01_C",
                   [TestFunc(btp.core_reg_svc_gap),
@@ -2383,7 +2392,7 @@ def test_cases(pts_bd_addr):
     """Returns a list of GATT test cases"""
 
     test_cases = test_cases_client(pts_bd_addr)
-    test_cases += test_cases_server()
+    test_cases = test_cases_server(pts_bd_addr)
 
     return test_cases
 

commit 0446205fc64bcce1d918b5a69300051bdaf0fee7

nble: fix GATT test cases regression from 8c20d920
List of GATT test cases contained only GATT server cases, GATT
client test cases were not included.

ptsprojects/nble/gatt.py
@@ -2408,7 +2408,7 @@ def test_cases(pts_bd_addr):
     """Returns a list of GATT test cases"""
 
     test_cases = test_cases_client(pts_bd_addr)
-    test_cases = test_cases_server(pts_bd_addr)
+    test_cases += test_cases_server(pts_bd_addr)
 
     return test_cases
 