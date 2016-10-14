ae07ce88fc2ee77ac8399de34f895ef371949f39
Mariusz Skamra <mariusz.skamra@tieto.com>
2016-10-14 10:03:01 +0200

    zephyr: Add TC_SEC_AUT_BV_23_C test case implementation
    This adds implememntation of TC_SEC_AUT_BV_23_C test case.

--- ptsprojects/zephyr/gap.py
+++ ptsprojects/zephyr/gap.py
@@ -801,6 +801,15 @@ def test_cases(pts):
                                  start_wid=112),
                         TestFunc(btp.gap_pair, pts_bd_addr, Addr.le_public,
                                  start_wid=108)]),
+        ZTestCase("GAP", "TC_SEC_AUT_BV_23_C",
+                  edit1_wids={1002: btp.var_store_get_passkey,
+                              144: "0008"},
+                  cmds=pre_conditions + init_gatt_db +
+                       [TestFunc(btp.gap_set_io_cap, IOCap.display_only),
+                        TestFunc(btp.gap_set_conn),
+                        TestFunc(btp.gap_adv_ind_on),
+                        TestFunc(btp.gap_connected_ev, post_wid=91),
+                        TestFunc(btp.gap_disconnected_ev, post_wid=1002)]),
          ZTestCase("GAP", "TC_SEC_CSIGN_BV_01_C",
                    pre_conditions +
                    [TestFunc(btp.core_reg_svc_gatts),
