c7f38951a11a21abecdaf1ba98ae91a9cfd66286
Ruslan Mstoi <ruslan.mstoi@linux.intel.com>
2017-04-26 17:58:30 +0300

    Set license: GPLv2 for GNU/Linux part, BSD for Windows part

--- tools/create-workspace.py
+++ tools/create-workspace.py
@@ -1,3 +1,18 @@
+#
+# auto-pts - The Bluetooth PTS Automation Framework
+#
+# Copyright (c) 2017, Intel Corporation.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+
 """Windows IronPython script to create PTS workspace
 
 The workspace is created using *.pts file from the Test Plan Generator.
