7aef8940b01138fcb9d79581189c9fddecfc61a8
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@codecoup.pl>
2017-10-26 10:36:44 +0200

    testcase: Introduce wid generic handler for profiles
    This handler will serve all wids for test case. Main idea of that
    handling is making test cases automated not step scripted. Actions of
    generic handler should be based on stack state and description content.

---
+++ ptsprojects/zephyr/mesh_wid.py
@@ -0,0 +1,34 @@
+#
+# auto-pts - The Bluetooth PTS Automation Framework
+#
+# Copyright (c) 2017, Intel Corporation.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+
+import logging
+import sys
+
+log = logging.debug
+
+def mesh_wid_hdl(wid, description):
+    log("%s, %r, %r", mesh_wid_hdl.__name__, wid, description)
+    module = sys.modules[__name__]
+
+    try:
+        handler = getattr(module, "hdl_wid_%d" % wid)
+        return handler(description)
+    except AttributeError:
+        log("wid nb: %d, not implemented!", wid)
+
+
+# wid handlers section begin
+def hdl_wid_12(desc):
+    pass
