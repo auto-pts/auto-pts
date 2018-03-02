auto-pts/auto-pts

commit ef6555a2b898780d1394f29e7db5920a8914ce2e

btp: Move device search to stack module
Move storage of found device from btp module to stack.

ptsprojects/stack.py
@@ -64,6 +64,8 @@ class Gap:
             "address": None,
             "type": None,
         })
+        self.discoverying = Property(False)
+        self.found_devices = Property([])  # List of found devices
 
     def wait_for_connection(self, timeout):
         if self.is_connected():
@@ -138,6 +140,9 @@ class Gap:
     def iut_has_privacy(self):
         return self.current_settings_get("Privacy")
 
+    def reset_discovery(self):
+        self.discoverying.data = True
+        self.found_devices.data = []
 
 class Mesh:
     def __init__(self, uuid, oob, output_size, output_actions, input_size,

pybtp/btp.py
@@ -44,7 +44,6 @@ PTS_BD_ADDR = LeAddress(addr_type=0, addr='000000000000')
 
 # Devices found
 LeAdv = namedtuple('LeAdv', 'addr_type addr rssi flags eir')
-DISCOV_RESULTS = []
 
 #  A sequence of values to verify in PTS MMI description
 VERIFY_VALUES = None
@@ -879,56 +878,8 @@ def gap_set_powered_off():
     __gap_current_settings_update(tuple_data)
 
 
-def __gap_device_found_timeout(continue_flag):
-    logging.debug("%s", __gap_device_found_timeout.__name__)
-    continue_flag.clear()
-
-
-def __gap_device_found_ev(duration):
-    logging.debug("%s %r", __gap_device_found_ev.__name__, duration)
-
-    iutctl = get_iut()
-
-    continue_flag = Event()
-    continue_flag.set()
-    t = Timer(duration, __gap_device_found_timeout, [continue_flag])
-    t.start()
-
-    while continue_flag.is_set():
-        try:
-            # Use 1 second socket timeout to check continue_flag every second
-            tuple_hdr, tuple_data = iutctl.btp_socket.read(1)
-        except socket.timeout:
-            continue
-
-        btp_hdr_check(tuple_hdr, defs.BTP_SERVICE_ID_GAP,
-                      defs.GAP_EV_DEVICE_FOUND)
-
-        fmt = '<B6sBBH'
-        if len(tuple_data[0]) < struct.calcsize(fmt):
-            raise BTPError("Invalid data length")
-
-        _addr_type, _addr, _rssi, _flags, _len = \
-            struct.unpack_from(fmt, tuple_data[0])
-        _eir = tuple_data[0][struct.calcsize(fmt):]
-
-        if len(_eir) != _len:
-            raise BTPError("Invalid data length")
-
-        _addr = binascii.hexlify(_addr[::-1]).lower()
-
-        logging.debug("found %r type %r", _addr, _addr_type)
-
-        global DISCOV_RESULTS
-        DISCOV_RESULTS.append(LeAdv(_addr_type, _addr, _rssi, _flags, _eir))
-
-
-def gap_start_discov(transport='le', type='active', mode='general',
-                     duration=10):
+def gap_start_discov(transport='le', type='active', mode='general'):
     """GAP Start Discovery function.
-
-    duration - Discovery duration in seconds (10 by default).
-               After this period of time discovery will be stopped.
 
     Possible options (key: <values>):
 
@@ -956,23 +907,15 @@ def gap_start_discov(transport='le', type='active', mode='general',
     elif mode == "observe":
         flags |= defs.GAP_DISCOVERY_FLAG_LE_OBSERVE
 
+    stack = get_stack()
+    stack.gap.reset_discovery()
+
     iutctl.btp_socket.send(*GAP['start_discov'], data=chr(flags))
 
     gap_command_rsp_succ()
 
-    # Make sure there are no previous results
-    global DISCOV_RESULTS
-    del DISCOV_RESULTS[:]
 
-    __gap_device_found_ev(duration)
-
-    # Stop discovery if expired
-    __gap_stop_discov()
-
-
-def check_discov_results(description, addr_type=None, addr=None,
-                         discovered=True, eir=None):
-
+def check_discov_results(addr_type=None, addr=None, discovered=True, eir=None):
     addr = pts_addr_get(addr)
     addr_type = pts_addr_type_get(addr_type)
 
@@ -981,23 +924,20 @@ def check_discov_results(description, addr_type=None, addr=None,
 
     found = False
 
-    global DISCOV_RESULTS
+    stack = get_stack()
+    devices = stack.gap.found_devices.data
 
-    while len(DISCOV_RESULTS):
-        result = DISCOV_RESULTS.pop()
-
-        if addr_type != result.addr_type:
+    for device in devices:
+        logging.debug("matching %r", device)
+        if addr_type != device.addr_type:
             continue
-        if addr != result.addr:
+        if addr != device.addr:
             continue
-        if eir and eir != result.eir:
+        if eir and eir != device.eir:
             continue
 
         found = True
         break
-
-    # Cleanup
-    del DISCOV_RESULTS[:]
 
     if discovered == found:
         return True
@@ -1005,32 +945,17 @@ def check_discov_results(description, addr_type=None, addr=None,
     return False
 
 
-def discover_and_verify(description, transport='le', type='active',
-                        mode='general', duration=10, addr=None,
-                        addr_type=None):
-    """Verify discovery results
-
-    This function verifies if the advertisement has been received and
-    optionally verifies the presence of specific eir data in received
-    advertisement
-
-    Returns True if verification is successful, False if not.
-
-    description -- MMI description
-    """
-    gap_start_discov(transport, type, mode, duration)
-
-    return check_discov_results(addr_type, addr)
-
-
-def __gap_stop_discov():
-    logging.debug("%s", __gap_stop_discov.__name__)
+def gap_stop_discov():
+    logging.debug("%s", gap_stop_discov.__name__)
 
     iutctl = get_iut()
 
     iutctl.btp_socket.send(*GAP['stop_discov'])
 
     gap_command_rsp_succ()
+
+    stack = get_stack()
+    stack.gap.discoverying.data = False
 
 
 def gap_read_ctrl_info():
@@ -2608,6 +2533,28 @@ def gap_new_settings_ev_(gap, data, data_len):
     __gap_current_settings_update(curr_set)
 
 
+def gap_device_found_ev_(gap, data, data_len):
+    logging.debug("%s %r", gap_device_found_ev_.__name__, data)
+
+    fmt = '<B6sBBH'
+    if len(data) < struct.calcsize(fmt):
+        raise BTPError("Invalid data length")
+
+    addr_type, addr, rssi, flags, eir_len = struct.unpack_from(fmt, data)
+    eir = data[struct.calcsize(fmt):]
+
+    if len(eir) != eir_len:
+        raise BTPError("Invalid data length")
+
+    addr = binascii.hexlify(addr[::-1]).lower()
+
+    logging.debug("found %r type %r eir %r", addr, addr_type, eir)
+
+    stack = get_stack()
+    stack.gap.found_devices.data.append(LeAdv(addr_type, addr, rssi, flags,
+                                              eir))
+
+
 def gap_connected_ev_(gap, data, data_len):
     logging.debug("%s %r", gap_connected_ev_.__name__, data)
 
@@ -2630,6 +2577,7 @@ def gap_disconnected_ev_(gap, data, data_len):
 
 GAP_EV = {
     defs.GAP_EV_NEW_SETTINGS:gap_new_settings_ev_,
+    defs.GAP_EV_DEVICE_FOUND: gap_device_found_ev_,
     defs.GAP_EV_DEVICE_CONNECTED: gap_connected_ev_,
     defs.GAP_EV_DEVICE_DISCONNECTED: gap_disconnected_ev_,
 }

commit 5b7eef89da655c087ff6fcace821e791c4c32afc

zephyr: Hotfix regression in GAP device discovery
This fixes regression introduced by
ef6555a2b898780d1394f29e7db5920a8914ce2e.
This refactors affected test cases to use generic wid handler.

ptsprojects/stack.py
@@ -38,7 +38,10 @@ def timeout_cb(flag):
 
 
 class Gap:
-    def __init__(self):
+    def __init__(self, name, manufacturer_data):
+        self.name = name
+        self.manufacturer_data = manufacturer_data
+
         # If disconnected - None
         # If connected - remote address tuple (addr, addr_type)
         self.connected = Property(None)
@@ -66,6 +69,8 @@ class Gap:
         })
         self.discoverying = Property(False)
         self.found_devices = Property([])  # List of found devices
+
+        self.passkey = Property(None)
 
     def wait_for_connection(self, timeout):
         if self.is_connected():
@@ -219,8 +224,8 @@ class Stack:
         self.gap = None
         self.mesh = None
 
-    def gap_init(self):
-        self.gap = Gap()
+    def gap_init(self, name=None, manufacturer_data=None):
+        self.gap = Gap(name, manufacturer_data)
 
     def mesh_init(self, uuid, oob, output_size, output_actions, input_size,
                   input_actions, crpl_size):
@@ -229,7 +234,7 @@ class Stack:
 
     def cleanup(self):
         if self.gap:
-            self.gap_init()
+            self.gap_init(self.gap.name, self.gap.manufacturer_data)
 
         if self.mesh:
             self.mesh_init(self.mesh.dev_uuid, self.mesh.static_auth,

ptsprojects/zephyr/gap.py
@@ -69,6 +69,7 @@ init_gatt_db=[TestFunc(btp.core_reg_svc_gatt),
               TestFunc(btp.gatts_start_server)]
 
 iut_device_name = 'Tester'
+iut_manufacturer_data = 'ABCD'
 
 
 class AdData:
@@ -91,12 +92,12 @@ def test_cases(pts):
 
     stack = get_stack()
 
-    stack.gap_init()
-
     # Set GAP common PIXIT values
     pts.update_pixit_param("GAP", "TSPX_delete_link_key", "TRUE")
 
     pre_conditions=[TestFunc(btp.core_reg_svc_gap),
+                    TestFunc(stack.gap_init, iut_device_name,
+                             iut_manufacturer_data),
                     TestFunc(btp.gap_read_ctrl_info),
                     TestFunc(lambda: pts.update_pixit_param(
                              "GAP", "TSPX_bd_addr_iut",
@@ -121,73 +122,34 @@ def test_cases(pts):
 
     test_cases = [
         ZTestCase("GAP", "GAP/BROB/BCST/BV-01-C",
-                  pre_conditions +
-                  [TestFunc(btp.gap_set_nonconn, start_wid=47),
-                   TestFunc(btp.gap_set_nondiscov, start_wid=47),
-                   TestFunc(btp.gap_adv_ind_on, start_wid=47)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/BROB/BCST/BV-02-C",
-                  pre_conditions +
-                  [TestFunc(btp.gap_set_nonconn),
-                   TestFunc(btp.gap_set_nondiscov),
-                   TestFunc(btp.gap_adv_ind_on, sd=[AdData.ad_manuf, AdData.ad_name_sh])]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/BROB/BCST/BV-03-C",
-                  edit1_wids={1002: (btp.var_store_get_passkey, pts_bd_addr,
-                                     Addr.le_public)},
                   cmds=pre_conditions +
                        [TestFunc(btp.gap_set_io_cap, IOCap.display_only),
-
-                        # Enter general discoverable mode and send connectable
-                        # event so that PTS could connect and get IRK
-                        TestFunc(btp.gap_set_conn),
-                        TestFunc(btp.gap_set_gendiscov),
-                        TestFunc(btp.gap_adv_ind_on,
-                                 sd=[AdData.ad_manuf, AdData.ad_name_sh]),
                         TestFunc(pts.update_pixit_param, "GAP",
                                  "TSPX_iut_device_name_in_adv_packet_for_random_address",
-                                 iut_device_name),
-                        TestFunc(btp.gap_wait_for_connection, post_wid=91),
-                        TestFunc(btp.gap_adv_off, post_wid=91),
-                        TestFunc(btp.gap_disconn, start_wid=77),
-
-                        # Enter broadcast mode
-                        TestFunc(btp.gap_set_nonconn, start_wid=80),
-                        TestFunc(btp.gap_set_nondiscov, start_wid=80),
-                        TestFunc(btp.gap_adv_ind_on,
-                                 sd=[AdData.ad_manuf, AdData.ad_name_sh],
-                                 start_wid=80)]),
+                                 iut_device_name)],
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/BROB/OBSV/BV-01-C",
-                  ok_cancel_wids={4: (btp.check_discov_results)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, type='passive',
-                                 mode='observe', post_wid=12)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/BROB/OBSV/BV-02-C",
-                  ok_cancel_wids={4: (btp.check_discov_results)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, type='active',
-                                 mode='observe', post_wid=169)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/BROB/OBSV/BV-05-C",
                   cmds=pre_conditions,
-                  verify_wids={157: (btp.discover_and_verify, 'le', 'active',
-                                     'observe')}),
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/BROB/OBSV/BV-06-C",
-                  edit1_wids={1002: (btp.var_store_get_passkey, pts_bd_addr,
-                                     Addr.le_public)},
                   cmds=pre_conditions +
                        [TestFunc(btp.gap_set_io_cap, IOCap.display_only),
-
                         # Set RPA update to 1 minute (60*1000=60000 ms)
                         TestFunc(pts.update_pixit_param, "GAP",
-                                 "TSPX_iut_private_address_interval", '60000'),
-
-                        # Connect and pair to get IRK
-                        TestFunc(btp.gap_conn, start_wid=78),
-                        TestFunc(btp.gap_pair, start_wid=108)],
-
-                  # Start Observation procedure to get Advertising Event with
-                  # resolved PTS address. Verify if advertisement was received.
-                  # The RPA shall be resolved to PTS public address
-                  verify_wids={138: (btp.discover_and_verify, 'le', 'active',
-                                     'observe')}),
+                                 "TSPX_iut_private_address_interval", '60000')],
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/NONM/BV-01-C",
                   pre_conditions +
                   [TestFunc(btp.gap_set_nonconn, start_wid=5),
@@ -218,76 +180,42 @@ def test_cases(pts):
                    TestFunc(btp.gap_set_gendiscov),
                    TestFunc(btp.gap_adv_ind_on, start_wid=52)]),
         ZTestCase("GAP", "GAP/DISC/LIMP/BV-01-C",
-                  ok_cancel_wids={10: (btp.check_discov_results)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, mode='limited',
-                                 post_wid=13)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/LIMP/BV-02-C",
-                  ok_cancel_wids={11: (btp.check_discov_results, None, None,
-                                       False)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, mode='limited',
-                                 post_wid=13)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/LIMP/BV-03-C",
-                  ok_cancel_wids={11: (btp.check_discov_results, None, None,
-                                       False)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, mode='limited',
-                                 post_wid=13)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/LIMP/BV-04-C",
-                  ok_cancel_wids={11: (btp.check_discov_results, None, None,
-                                       False)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, mode='limited',
-                                 post_wid=13)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/LIMP/BV-05-C",
-                  ok_cancel_wids={11: (btp.check_discov_results, None, None,
-                                       False)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, mode='limited',
-                                 post_wid=13)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/GENP/BV-01-C",
-                  ok_cancel_wids={14: (btp.check_discov_results)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, post_wid=23)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/GENP/BV-02-C",
-                  ok_cancel_wids={14: (btp.check_discov_results)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, post_wid=23)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/GENP/BV-03-C",
-                  ok_cancel_wids={11: (btp.check_discov_results, None, None,
-                                       False)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, post_wid=23)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/GENP/BV-04-C",
-                  ok_cancel_wids={11: (btp.check_discov_results, None, None,
-                                       False)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, post_wid=23)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/GENP/BV-05-C",
-                  ok_cancel_wids={11: (btp.check_discov_results, None, None,
-                                       False)},
-                  cmds=pre_conditions +
-                       [TestFunc(btp.gap_start_discov, post_wid=23)]),
+                  cmds=pre_conditions,
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/DISC/RPA/BV-01-C",
-                  edit1_wids={1002: (btp.var_store_get_passkey, pts_bd_addr,
-                                     Addr.le_public)},
                   cmds=pre_conditions +
                        [TestFunc(btp.gap_set_io_cap, IOCap.display_only),
-
                         # Set RPA update to 1 minute (60*1000=60000 ms)
                         TestFunc(pts.update_pixit_param, "GAP",
-                                 "TSPX_iut_private_address_interval", '60000'),
-
-                        # Connect and pair to get IRK
-                        TestFunc(btp.gap_conn, start_wid=78),
-                        TestFunc(btp.gap_pair, start_wid=108)],
-
-                  # Start Discovery procedure to get Advertising Event with
-                  # resolved PTS address. Verify if advertisement was received.
-                  # The RPA shall be resolved to PTS public address
-                  verify_wids={138: (btp.discover_and_verify, 'le', 'active',
-                                     'general')}),
+                                 "TSPX_iut_private_address_interval", '60000')],
+                  generic_wid_hdl=gap_wid_hdl),
         ZTestCase("GAP", "GAP/IDLE/NAMP/BV-01-C",
                   pre_conditions +
                   [TestFunc(btp.core_reg_svc_gatt),

ptsprojects/zephyr/gap_wid.py
@@ -16,10 +16,12 @@
 import logging
 import sys
 from pybtp import btp
-from pybtp.types import Prop, Perm, UUID
+from pybtp.types import Prop, Perm, UUID, AdType
 import re
 import struct
 from ptsprojects.stack import get_stack
+from binascii import hexlify
+from time import sleep
 
 log = logging.debug
 
@@ -36,14 +38,112 @@ def gap_wid_hdl(wid, description):
 
 
 # wid handlers section begin
+def hdl_wid_4(desc):
+    sleep(10)  # Give some time to discover devices
+    btp.gap_stop_discov()
+    return btp.check_discov_results()
+
+
+def hdl_wid_5(desc):
+    stack = get_stack()
+
+    ad = []
+    sd = []
+
+    btp.gap_set_nonconn()
+    btp.gap_set_nondiscov()
+
+    if stack.gap.name:
+        ad.append((AdType.name_short, hexlify(stack.gap.name)))
+
+    if stack.gap.manufacturer_data:
+        sd.append((AdType.manufacturer_data, stack.gap.manufacturer_data))
+
+    btp.gap_adv_ind_on(ad=ad, sd=sd)
+    return True
+
+
+def hdl_wid_10(desc):
+    btp.gap_stop_discov()
+    return btp.check_discov_results(discovered=True)
+
+
+def hdl_wid_11(desc):
+    btp.gap_stop_discov()
+    return btp.check_discov_results(discovered=False)
+
+
+def hdl_wid_12(desc):
+    btp.gap_start_discov(type='passive', mode='observe')
+    return True
+
+
+def hdl_wid_13(desc):
+    btp.gap_start_discov(mode='limited')
+    return True
+
+
+def hdl_wid_14(desc):
+    btp.gap_stop_discov()
+    return btp.check_discov_results(discovered=True)
+
+
+def hdl_wid_23(desc):
+    btp.gap_start_discov()
+    return True
+
+
+def hdl_wid_47(desc):
+    btp.gap_set_nonconn()
+    btp.gap_set_nondiscov()
+    btp.gap_adv_ind_on()
+    return True
+
+
 def hdl_wid_77(desc):
     btp.gap_disconn()
     return True
 
 
+def hdl_wid_78(desc):
+    btp.gap_conn()
+    return True
+
+
+def hdl_wid_80(desc):
+    stack = get_stack()
+
+    ad = []
+    sd = []
+
+    btp.gap_set_nonconn()
+    btp.gap_set_nondiscov()
+
+    if stack.gap.name:
+        ad.append((AdType.name_short, hexlify(stack.gap.name)))
+
+    if stack.gap.manufacturer_data:
+        sd.append((AdType.manufacturer_data, stack.gap.manufacturer_data))
+
+    btp.gap_adv_ind_on(ad=ad, sd=sd)
+    return True
+
+
 def hdl_wid_91(desc):
+    stack = get_stack()
+
+    ad = []
+    sd = []
+
     btp.gap_set_conn()
-    btp.gap_adv_ind_on()
+
+    if stack.gap.name:
+        ad.append((AdType.name_short, hexlify(stack.gap.name)))
+
+    if stack.gap.manufacturer_data:
+        sd.append((AdType.manufacturer_data, stack.gap.manufacturer_data))
+
+    btp.gap_adv_ind_on(ad=ad, sd=sd)
     return True
 
 
@@ -83,8 +183,22 @@ def hdl_wid_136(desc):
     return True
 
 
+def hdl_wid_138(desc):
+    btp.gap_start_discov(transport='le', type='active', mode='observe')
+    sleep(10)  # Give some time to discover devices
+    btp.gap_stop_discov()
+    return btp.check_discov_results()
+
+
 def hdl_wid_141(desc):
     return btp.gatts_verify_write_success(desc)
+
+
+def hdl_wid_157(desc):
+    btp.gap_start_discov(transport='le', type='active', mode='observe')
+    sleep(10)  # Give some time to discover devices
+    btp.gap_stop_discov()
+    return btp.check_discov_results()
 
 
 def hdl_wid_161(desc):
@@ -122,3 +236,15 @@ def hdl_wid_161(desc):
 
     (att_rsp, val_len, val) = value
     return val_len
+
+
+def hdl_wid_169(desc):
+    btp.gap_start_discov(type='active', mode='observe')
+    return True
+
+
+def hdl_wid_1002(desc):
+    stack = get_stack()
+    passkey = stack.gap.passkey.data
+    stack.gap.passkey.data = None
+    return passkey

pybtp/btp.py
@@ -945,24 +945,6 @@ def check_discov_results(addr_type=None, addr=None, discovered=True, eir=None):
     return False
 
 
-def discover_and_verify(description, transport='le', type='active',
-                        mode='general', duration=10, addr=None,
-                        addr_type=None):
-    """Verify discovery results
-
-    This function verifies if the advertisement has been received and
-    optionally verifies the presence of specific eir data in received
-    advertisement
-
-    Returns True if verification is successful, False if not.
-
-    description -- MMI description
-    """
-    gap_start_discov(transport, type, mode, duration)
-
-    return check_discov_results(addr_type, addr)
-
-
 def gap_stop_discov():
     logging.debug("%s", gap_stop_discov.__name__)
 
@@ -2593,11 +2575,25 @@ def gap_disconnected_ev_(gap, data, data_len):
     gap.connected.data = None
 
 
+def gap_passkey_disp_ev_(gap, data, data_len):
+    logging.debug("%s %r", gap_passkey_disp_ev_.__name__, data)
+
+    fmt = '<B6sI'
+
+    addr_type, addr, passkey = struct.unpack(fmt, data)
+    addr = binascii.hexlify(addr[::-1])
+
+    logging.debug("passkey = %r", passkey)
+
+    gap.passkey.data = passkey
+
+
 GAP_EV = {
     defs.GAP_EV_NEW_SETTINGS:gap_new_settings_ev_,
     defs.GAP_EV_DEVICE_FOUND: gap_device_found_ev_,
     defs.GAP_EV_DEVICE_CONNECTED: gap_connected_ev_,
     defs.GAP_EV_DEVICE_DISCONNECTED: gap_disconnected_ev_,
+    defs.GAP_EV_PASSKEY_DISPLAY: gap_passkey_disp_ev_,
 }
 
 