b5f9df70eaaa2cca139b2ef86748c0adf8bdcdd6
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2019-04-17 10:12:54 +0200

    parser: Code cleanup
    * Remove unused code
    * Remove redundant logging

--- pybtp/parser.py
+++ pybtp/parser.py
@@ -14,31 +14,9 @@
 #
 
 import struct
-import defs
 from collections import namedtuple
-import logging
 
 HDR_LEN = 5
-
-
-# Service frames parsers
-def parse_svc_core(op, data_len, data):
-    if op not in CORE_SERVICE_OP.itervalues():
-        raise Exception("Wrong Core Service OP of received frame")
-    pass
-
-
-def parse_svc_gap(op, data_len, data):
-    pass
-
-
-def parse_frame_generic(hdr, data):
-    if hdr.svc_id == defs.BTP_SERVICE_ID_CORE:
-        parse_svc_core(hdr.op, hdr.data_len, data)
-    elif hdr.svc_id == defs.BTP_SERVICE_ID_GAP:
-        parse_svc_gap(hdr.op, hdr.data_len, data)
-    else:
-        raise Exception("Wrong Service ID of received frame")
 
 
 def dec_hdr(bin):
@@ -51,32 +29,21 @@ def dec_hdr(bin):
     +------------+--------+------------------+-------------+
 
     """
-    logging.debug("%s, %r", dec_hdr.__name__, bin)
-
     Header = namedtuple('Header', 'svc_id op ctrl_index data_len')
 
-    hdr = Header._make(struct.unpack("<BBBH", bin))
-
-    return hdr
+    return Header._make(struct.unpack("<BBBH", bin))
 
 
 def dec_data(bin):
-    logging.debug("%s, %r", dec_data.__name__, bin)
-
     data_len = len(bin)
-    data = struct.unpack('<%ds' % data_len, bin)
 
-    return data
+    return struct.unpack('<%ds' % data_len, bin)
 
 
 def enc_frame(svc_id, op, ctrl_index, data):
-    logging.debug("%s, %r %r %r %r",
-                  enc_frame.__name__, svc_id, op, ctrl_index, data)
-
     str_data = str(bytearray(data))
     int_len = len(str_data)
     hex_len = struct.pack('h', int_len)
-    bin = struct.pack('<BBB2s%ds' % int_len, svc_id, op, ctrl_index, hex_len,
-                      str_data)
 
-    return bin
+    return struct.pack('<BBB2s%ds' % int_len, svc_id, op, ctrl_index, hex_len,
+                       str_data)
