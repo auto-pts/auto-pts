auto-pts/auto-pts

commit a3768cc09d35b6d89853037110d0d99655222abb

bot: Use git-describe for source info
There was a request from Zephyr team to use git-describe for source info
instead of raw commit-id.

Fixes: #300

Signed-off-by: Tedd Ho-Jeong An <tedd.an@intel.com>

bot/common.py
@@ -361,7 +361,7 @@ def update_sources(repo, remote, branch, stash_changes=False):
 
     if dirty and (not stash_changes):
         print('Repo is dirty. Not updating')
-        return repo.git.show('-s', '--format=%H') + '-dirty'
+        return repo.git.describe(), repo.git.show('-s', '--format=%H') + '-dirty'
 
     if dirty and stash_changes:
         print('Repo is dirty. Stashing changes')
@@ -370,30 +370,33 @@ def update_sources(repo, remote, branch, stash_changes=False):
     repo.git.fetch(remote)
     repo.git.checkout('{}/{}'.format(remote, branch))
 
-    return repo.git.show('-s', '--format=%H')
+    return repo.git.describe(), repo.git.show('-s', '--format=%H')
 
 
 def update_repos(project_path, git_config):
     """GIT Update sources
     :param project_path: path to project root
     :param git_config: dictionary with configuration of repositories
-    :return: status_dict with {key=repo name, value=status}
+    :return: repos_dict with {key=repo name, {commit, desc}}
     """
     project_path = os.path.abspath(project_path)
-    status_dict = {}
+    repos_dict = {}
 
     for repo, conf in git_config.iteritems():
+        repo_dict = {}
         if not os.path.isabs(conf["path"]):
             repo_path = os.path.join(project_path, conf["path"])
         else:
             repo_path = os.path.abspath(conf["path"])
 
         project_path.join(repo_path)
-        status = update_sources(repo_path, conf["remote"], conf["branch"],
-                                conf["stash_changes"])
-        status_dict[repo] = status
+        desc, commit = update_sources(repo_path, conf["remote"],
+                                      conf["branch"], conf["stash_changes"])
+        repo_dict["commit"] = commit
+        repo_dict["desc"] = desc
+        repos_dict[repo] = repo_dict
 
-    return status_dict
+    return repos_dict
 
 
 def cleanup():

bot/zephyr.py
@@ -424,8 +424,11 @@ def main(cfg):
         reg_html = bot.common.regressions2html(_regressions)
 
         # Zephyr commit id link in HTML format
-        zephyr_hash_html = bot.common.url2html(zephyr_hash_url(zephyr_hash),
-                                               zephyr_hash)
+
+        # Commit id may have "-dirty" if the source is dirty.
+        commit_id = zephyr_hash["commit"].split('-')[0]
+        zephyr_hash_html = \
+            bot.common.url2html(zephyr_hash_url(commit_id), zephyr_hash["desc"])
 
         # Log in Google drive in HTML format
         if 'gdrive' in cfg:

commit c05ad96d4ed8a0cc7466bc62971bc9bc9799aa13

bot/common: Add '--always' flag to git describe to suppress errors
git describe --always show uniquely abbreviated commit object as fallback
when there are no annotated tags to use.

This fixes a regression introduced in:
a3768cc bot: Use git-describe for source info

bot/common.py
@@ -398,7 +398,8 @@ def update_sources(repo, remote, branch, stash_changes=False):
 
     if dirty and (not stash_changes):
         print('Repo is dirty. Not updating')
-        return repo.git.describe(), repo.git.show('-s', '--format=%H') + '-dirty'
+        return repo.git.describe('--always'), \
+               repo.git.show('-s', '--format=%H') + '-dirty'
 
     if dirty and stash_changes:
         print('Repo is dirty. Stashing changes')
@@ -407,7 +408,8 @@ def update_sources(repo, remote, branch, stash_changes=False):
     repo.git.fetch(remote)
     repo.git.checkout('{}/{}'.format(remote, branch))
 
-    return repo.git.describe(), repo.git.show('-s', '--format=%H')
+    return repo.git.describe('--always'), \
+           repo.git.show('-s', '--format=%H')
 
 
 def update_repos(project_path, git_config):