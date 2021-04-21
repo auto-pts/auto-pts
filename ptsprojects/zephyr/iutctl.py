a2e5b8bfb87d9ca03bd13a68ad46b43f5b509bcd
Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>
2021-04-21 14:23:18 +0200

    zephyr: Bind btp socket to /tmp/bt-stack-testerxxxxxxxxx
    to allow many instances of autoptsclient.
    Append jlink serial number to the name /tmp/bt-stack-tester,
    so it will look like /tmp/bt-stack-testerxxxxxxxxx and many
    instances of autoptsclient will not share the same file.

--- ptsprojects/zephyr/iutctl.py
+++ ptsprojects/zephyr/iutctl.py
@@ -75,7 +75,7 @@ class ZephyrCtl:
         self.test_case = None
         self.rtt2pty_process = None
         self.iut_log_file = None
-        self.btp_address = BTP_ADDRESS
+        self.btp_address = BTP_ADDRESS + self.debugger_snr
 
         if use_rtt2pty:
             self.rtt2pty = RTT2PTY()
