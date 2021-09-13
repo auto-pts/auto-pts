ec38b4379632269f84ea5e290004d2e606f7d6fa
Krzysztof Kopyściński <krzysztof.kopyscinski@codecoup.pl>
2021-09-13 14:50:20 +0200

    Mynewt: fix GATT/CL/GAR tests
    All of these tests require gatt_read, not gatt_read_long. Added separate
    test handler them.

--- ptsprojects/mynewt/gattc_wid.py
+++ ptsprojects/mynewt/gattc_wid.py
@@ -36,6 +36,14 @@ def gattc_wid_hdl(wid, description, test_case_name):
         return gatt_wid_hdl(wid, description, test_case_name)
 
 
+def gattc_wid_hdl_no_read_long(wid, description, test_case_name):
+    if wid == 48:
+        log("%s, %r, %r, %s", gattc_wid_hdl_no_read_long.__name__, wid, description,
+            test_case_name)
+        return hdl_wid_48_no_long_read(description)
+    return gattc_wid_hdl(wid, description, test_case_name)
+
+
 def hdl_wid_10(desc):
     btp.gattc_disc_all_prim(btp.pts_addr_type_get(None),
                             btp.pts_addr_get(None))
@@ -49,6 +57,22 @@ def hdl_wid_17(desc):
 
 def hdl_wid_24(desc):
     return btp.verify_description(desc)
+
+
+def hdl_wid_48_no_long_read(desc):
+    MMI.reset()
+    MMI.parse_description(desc)
+
+    hdl = MMI.args[0]
+
+    if not hdl:
+        logging.debug("parsing error")
+        return False
+
+    btp.gattc_read(btp.pts_addr_type_get(), btp.pts_addr_get(), hdl)
+
+    btp.gattc_read_rsp(True, True)
+    return True
 
 
 def hdl_wid_48(desc):
@@ -74,3 +98,24 @@ def hdl_wid_48(desc):
 
 def hdl_wid_52(desc):
     return btp.verify_description(desc)
+
+
+def hdl_wid_58(desc):
+    MMI.reset()
+    MMI.parse_description(desc)
+
+    hdl = MMI.args[0]
+
+    if not hdl:
+        logging.debug("parsing error")
+        return False
+
+    btp.gattc_read_long(btp.pts_addr_type_get(None), btp.pts_addr_get(None),
+                        hdl, 0, 1)
+
+    try:
+        btp.gattc_read_long_rsp(True, True)
+    except socket.timeout:
+        pass
+
+    return True
