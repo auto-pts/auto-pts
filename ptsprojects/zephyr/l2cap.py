327291fc97bad68c8fe63ccfe28712ea51e29d98
Szymon Janc <szymon.janc@codecoup.pl>
2021-11-08 11:30:07 +0100

    zephyr: Add support for L2CAP/LE/CFC/BV-25-C
    This test requires to reply with Insufficient Encryption but PTS is not
    pairing to trigger initial conditions, hence errata.

--- ptsprojects/zephyr/l2cap.py
+++ ptsprojects/zephyr/l2cap.py
@@ -198,6 +198,9 @@ def test_cases(ptses):
                   pre_conditions +
                   [TestFunc(lambda: btp.l2cap_le_listen(le_psm))],
                   generic_wid_hdl=l2cap_wid_hdl),
+        ZTestCase("L2CAP", "L2CAP/LE/CFC/BV-25-C",
+                  pre_conditions_authen,
+                  generic_wid_hdl=l2cap_wid_hdl),
         # Enhanced Credit Based Flow Control Channel
         ZTestCase("L2CAP", "L2CAP/ECFC/BV-07-C",
                   pre_conditions_eatt,
