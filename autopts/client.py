4209c55613b53c47ff99f0e28f856f07f0afce1f
Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>
2023-05-17 11:41:38 +0200

    autoptsserver: Refactor stopping PTS and dongle selection
    Race in PID detection could terminate the wrong autoptsserver instance.
    After this refactor, we search for the PID of PTS.exe using the window
    title, as the address is placed there after a dongle selection. However,
    this method can fail if no dongle was selected. Then ExitPTS method of
    COM server will be used. But this can fail too. Then "kill-them-all"
    procedure will be berformed, but only if there is only a single instance
    of autoptsserver.py on the host machine.

--- autopts/client.py
+++ autopts/client.py
@@ -1214,10 +1214,17 @@ def run_recovery(args, ptses):
 
     try:
         for pts in ptses:
-            pts.request_recovery()
+            pts.request_pts_recovery()
     except Exception as e:
         logging.exception(e)
         traceback.print_exc()
+
+        try:
+            for pts in ptses:
+                pts.request_server_recovery()
+        except Exception as e:
+            logging.exception(e)
+            traceback.print_exc()
 
     for pts in ptses:
         wait_for_server_restart(pts)
