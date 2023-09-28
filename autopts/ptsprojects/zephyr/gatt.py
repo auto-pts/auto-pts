4307eea365a30d8f730905d3dc9dfc48d99e3a9f
Szymon Janc <szymon.janc@codecoup.pl>
2023-09-28 15:15:30 +0200

    zephyr: Prefer EATT for GATT tests
    Make PTS use EATT for running GATT tests.

--- autopts/ptsprojects/zephyr/gatt.py
+++ autopts/ptsprojects/zephyr/gatt.py
@@ -117,6 +117,7 @@ def set_pixits(ptses):
     pts.set_pixit("GATT", "TSPX_use_dynamic_pin", "FALSE")
     pts.set_pixit("GATT", "TSPX_delete_ltk", "TRUE")
     pts.set_pixit("GATT", "TSPX_tester_appearance", "0000")
+    pts.set_pixit("GATT", "TSPX_bearer_for_le", "EATT")
 
     if len(ptses) < 2:
         return
