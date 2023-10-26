auto-pts/auto-pts

commit 1105383ced7a3c58d9c6f22b8a360a4b21230cd1

bot: client: Fix missing server_count and server_args
Bot arguments was missing in a client.

autopts/client.py
@@ -472,15 +472,16 @@ def init_pts(args, ptses, tc_db_table_name=None):
     finish_count = CounterWithFlag(init_count=0)
 
     init_logging('_' + '_'.join(str(x) for x in args.cli_port))
+    server_count = getattr(args, 'server_count',  len(args.cli_port))
 
     # PtsServer.finish_count.clear()
-    for i in range(0, args.server_count):
+    for i in range(0, server_count):
         if i < len(proxy_list):
             proxy = proxy_list[i]
         else:
             if AUTO_PTS_LOCAL:
                 proxy = FakeProxy()
-            elif args.server_args:
+            elif hasattr(args, 'server_args'):
                 proxy = PtsServer.factory_get_instance(args.server_args[i])
             else:
                 proxy = PtsServerProxy.factory_get_instance(

commit e7360ead61a38015982681362f9a1edabc2b21d2

bot: Do not select builtin server if server_args is None
Mistake introduced with 1105383ced7a3c58d9c6f22b8a360a4b21230cd1

autopts/client.py
@@ -481,7 +481,7 @@ def init_pts(args, ptses, tc_db_table_name=None):
         else:
             if AUTO_PTS_LOCAL:
                 proxy = FakeProxy()
-            elif hasattr(args, 'server_args'):
+            elif getattr(args, 'server_args', False):
                 proxy = PtsServer.factory_get_instance(args.server_args[i])
             else:
                 proxy = PtsServerProxy.factory_get_instance(