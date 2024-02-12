ff1062ec16d1b94aeaedf980869c77f9346f2b16
Aytürk Düzen <ayturk.duzen@nordicsemi.no>
2024-02-12 11:57:38 +0100

    boards: add nRF54L15 device
    nRF54L15 SoC device added.
    
    Signed-off-by: Aytürk Düzen <ayturk.duzen@nordicsemi.no>

---
+++ autopts/ptsprojects/boards/nrf54l.py
@@ -0,0 +1,20 @@
+#
+# auto-pts - The Bluetooth PTS Automation Framework
+#
+# Copyright (c) 2024, Nordic Semiconductor ASA.
+# Copyright (c) 2024, Codecoup.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+
+from .nrf5x import *
+
+board_type = 'nrf54l15pdk_nrf54l15_cpuapp'
+
