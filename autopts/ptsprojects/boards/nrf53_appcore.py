951097a01d732323e4246ae57c869c8bc480d07f
Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>
2024-08-23 13:23:21 +0200

    boards: Add config for building/flashing only appcore of nrf53
    If someone would like to use a different controller and make the bot to
    build/flash only appcore of nrf53 board, all they need to do from now
    is to set this option "board": "nrf53_appcore" in the bot's config.py file.

---
+++ autopts/ptsprojects/boards/nrf53_appcore.py
@@ -0,0 +1,43 @@
+#
+# auto-pts - The Bluetooth PTS Automation Framework
+#
+# Copyright (c) 2023, Codecoup.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+
+from .nrf5x import *
+from autopts.bot.common import check_call
+
+board_type = 'nrf5340dk/nrf5340/cpuapp'
+
+
+def build_and_flash(zephyr_wd, board, debugger_snr, conf_file=None, *args):
+    """Build and flash Zephyr binary
+    :param zephyr_wd: Zephyr source path
+    :param board: IUT
+    :param debugger_snr serial number
+    :param conf_file: configuration file to be used
+    """
+    logging.debug("%s: %s %s %s", build_and_flash.__name__, zephyr_wd,
+                  board, conf_file)
+
+    tester_dir = os.path.join(zephyr_wd, 'tests', 'bluetooth', 'tester')
+
+    check_call('rm -rf build/'.split(), cwd=tester_dir)
+
+    bttester_overlay = 'nrf5340_hci_ipc.conf'
+
+    if conf_file and conf_file != 'default' and conf_file != 'prj.conf':
+        bttester_overlay += f';{conf_file}'
+
+    cmd = ['west', 'build', '-b', board, '--', f'-DOVERLAY_CONFIG=\'{bttester_overlay}\'']
+    check_call(cmd, cwd=tester_dir)
+    check_call(['west', 'flash', '--skip-rebuild', '--recover', '-i', debugger_snr], cwd=tester_dir)
