373a68004b853e5eaf74e6da81aa1af71ee27f01
Szymon Janc <szymon.janc@codecoup.pl>
2024-10-24 08:54:49 +0200

    wid: tmap: Use raw string for regex
    Fix following:
    autopts/wid/tmap.py:718: SyntaxWarning: invalid escape sequence '\d'

--- autopts/wid/tmap.py
+++ autopts/wid/tmap.py
@@ -715,7 +715,7 @@ def hdl_wid_2004(params: WIDParams):
     """
     Please confirm that 6 digit number is matched with [passkey].
     """
-    pattern = '[\d]{6}'
+    pattern = r'[\d]{6}'
     passkey = re.search(pattern, params.description)[0]
     stack = get_stack()
     bd_addr = btp.pts_addr_get()
