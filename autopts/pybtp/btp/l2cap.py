580c53a1aa81469f0af9010deaee7314276b8188
Lyle Zhu <lyle.zhu@nxp.com>
2024-11-28 19:26:28 +0800

    L2CAP: add command l2cap_listen_with_mode
    Add command `l2cap_listen_with_mode` to listen with the specified mode.
    
    Unlike `l2cap_listen`, `l2cap_listen_with_mode` adds a parameter `opt`
    to configure the mode.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/pybtp/btp/l2cap.py
+++ autopts/pybtp/btp/l2cap.py
@@ -51,6 +51,8 @@ L2CAP = {
                    CONTROLLER_INDEX),
     "cls_send": (defs.BTP_SERVICE_ID_L2CAP, defs.BTP_L2CAP_CMD_CLS_SEND,
                  CONTROLLER_INDEX),
+    "listen_with_mode": (defs.BTP_SERVICE_ID_L2CAP, defs.BTP_L2CAP_CMD_LISTEN_WITH_MODE,
+                         CONTROLLER_INDEX),
 }
 
 
@@ -396,6 +398,25 @@ def l2cap_cls_send(bd_addr, bd_addr_type, psm, val=None, val_mtp=None):
         data_ba.extend(val_ba)
 
     iutctl.btp_socket.send_wait_rsp(*L2CAP['cls_send'], data=data_ba)
+
+
+def l2cap_listen_with_mode(psm, transport, opt=defs.L2CAP_LISTEN_OPT_NONE, mtu=0, response=L2CAPConnectionResponse.success):
+    logging.debug("%s %r %r %r %r %r", l2cap_listen_with_mode.__name__, psm, transport, opt, mtu, response)
+
+    iutctl = get_iut()
+
+    if isinstance(psm, str):
+        psm = int(psm, 16)
+
+    data_ba = bytearray(struct.pack('H', psm))
+    data_ba.extend(struct.pack('B', transport))
+    data_ba.extend(struct.pack('B', opt))
+    data_ba.extend(struct.pack('H', mtu))
+    data_ba.extend(struct.pack('H', response))
+
+    iutctl.btp_socket.send(*L2CAP['listen_with_mode'], data=data_ba)
+
+    l2cap_command_rsp_succ(defs.BTP_L2CAP_CMD_LISTEN_WITH_MODE)
 
 
 def l2cap_connected_ev(l2cap, data, data_len):
