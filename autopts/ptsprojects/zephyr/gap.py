4405a5bcceaef3665470ae301c3eb5357f5a1906
Lyle Zhu <lyle.zhu@nxp.com>
2025-03-04 21:05:24 +0800

    Zephyr: Enable L2CAP service for case GAP/IDLE/BON/BV-06-C
    Register L2CAP Service.
    Initialize L2CAP Service.
    Change IO capability to `IOCap.display_yesno`.
    
    Update wid 102 to disable GAP pairing procedure.
    
    Update wid 2001 to change the address type to `defs.BTP_BR_ADDRESS_TYPE`.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/gap.py
+++ autopts/ptsprojects/zephyr/gap.py
@@ -282,6 +282,11 @@ def test_cases(ptses):
         ZTestCase("GAP", "GAP/IDLE/BON/BV-05-C",
                   cmds=br_l2cap,
                   generic_wid_hdl=gap_wid_hdl),
+        ZTestCase("GAP", "GAP/IDLE/BON/BV-06-C",
+                  cmds=br_l2cap + [
+                      TestFunc(btp.gap_set_io_cap, IOCap.display_yesno),
+                  ],
+                  generic_wid_hdl=gap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('GAP')
