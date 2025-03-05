cc413ef14cfb4f92b3d5aeca661e9ab75ce17b84
Lyle Zhu <lyle.zhu@nxp.com>
2025-03-05 11:06:46 +0800

    Zephyr: Enable L2CAP service for case GAP/SEC/SEM/BV-08-C
    Change IO capability to `IOCap.no_input_output`.
    Register L2CAP Service.
    Initialize L2CAP Service.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/gap.py
+++ autopts/ptsprojects/zephyr/gap.py
@@ -295,6 +295,9 @@ def test_cases(ptses):
                       TestFunc(btp.gap_set_gendiscov),
                   ],
                   generic_wid_hdl=gap_wid_hdl),
+        ZTestCase("GAP", "GAP/SEC/SEM/BV-08-C",
+                  cmds=br_l2cap,
+                  generic_wid_hdl=gap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('GAP')
