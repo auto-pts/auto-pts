aafa74f99c3151644d58a65a69a28671eda2dd4b
Lyle Zhu <lyle.zhu@nxp.com>
2025-03-07 23:51:04 +0800

    Zephyr: Support case GAP/SEC/SEM/BV-49-C
    Register L2CAP Service.
    Initialize L2CAP Service.
    Set IO capability to `display_yesno`.
    Start a L2CAP server with response type
    `L2CAPConnectionResponse.insufficient_secure_authentication`.
    
    Update wid 2001 to change the address type to
    `defs.BTP_BR_ADDRESS_TYPE` for the case.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/gap.py
+++ autopts/ptsprojects/zephyr/gap.py
@@ -407,6 +407,11 @@ def test_cases(ptses):
                       TestFunc(btp.gap_set_io_cap, IOCap.display_yesno),
                   ],
                   generic_wid_hdl=gap_wid_hdl),
+        ZTestCase("GAP", "GAP/SEC/SEM/BV-49-C",
+                  cmds=br_l2cap_secure_authen + [
+                      TestFunc(btp.gap_set_io_cap, IOCap.display_yesno),
+                  ],
+                  generic_wid_hdl=gap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('GAP')
