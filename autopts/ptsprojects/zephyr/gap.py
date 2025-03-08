28968547431cf70a08c45af18d5aa73abb66429a
Lyle Zhu <lyle.zhu@nxp.com>
2025-03-08 20:18:32 +0800

    Zephyr: Support case GAP/SEC/SEM/BI-02-C
    Register L2CAP Service.
    Initialize L2CAP Service.
    Set IO capability to `no_input_output`.
    Start a L2CAP server with response type
    `L2CAPConnectionResponse.insufficient_encryption`.
    
    Update wid 102 to bypass the ACL connection create procedure.
    
    Add wid 257.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/gap.py
+++ autopts/ptsprojects/zephyr/gap.py
@@ -453,6 +453,9 @@ def test_cases(ptses):
         ZTestCase("GAP", "GAP/SEC/SEM/BI-12-C",
                   cmds=br_l2cap,
                   generic_wid_hdl=gap_wid_hdl),
+        ZTestCase("GAP", "GAP/SEC/SEM/BI-02-C",
+                  cmds=br_l2cap_success,
+                  generic_wid_hdl=gap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('GAP')
