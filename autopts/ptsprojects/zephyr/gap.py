6ad9dc03e33c27fe180204ec080fbd1658b0ada2
Lyle Zhu <lyle.zhu@nxp.com>
2025-03-09 21:00:45 +0800

    Zephyr: Support case GAP/SEC/SEM/BV-25-C
    Register L2CAP Service.
    Initialize L2CAP Service.
    Set IO capability to `display_yesno`.
    
    Update wid 102 to wait for confirm passkey after pairing procedure
    started for this case.
    
    Enable SC only mode for this case.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/gap.py
+++ autopts/ptsprojects/zephyr/gap.py
@@ -584,6 +584,11 @@ def test_cases(ptses):
         ZTestCase("GAP", "GAP/SEC/SEM/BI-25-C",
                   cmds=br_l2cap,
                   generic_wid_hdl=gap_wid_hdl),
+        ZTestCase("GAP", "GAP/SEC/SEM/BV-25-C",
+                  cmds=br_l2cap + [
+                      TestFunc(btp.gap_set_io_cap, IOCap.display_yesno),
+                  ],
+                  generic_wid_hdl=gap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('GAP')
