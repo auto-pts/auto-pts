b044c3e09c94b36435882e717e3f5e3f6dec0f75
Lyle Zhu <lyle.zhu@nxp.com>
2025-04-22 20:19:01 +0800

    Zephyr: Support case L2CAP/COS/RTX/BV-02-C
    Set IO capability to `no_input_output`.
    
    Start a L2CAP server with the PSM=0x1001.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/l2cap.py
+++ autopts/ptsprojects/zephyr/l2cap.py
@@ -339,6 +339,9 @@ def test_cases(ptses):
         ZTestCase("L2CAP", "L2CAP/COS/RTX/BV-01-C",
                   br_l2cap_success_ret,
                   generic_wid_hdl=l2cap_wid_hdl),
+        ZTestCase("L2CAP", "L2CAP/COS/RTX/BV-02-C",
+                  br_l2cap_success_ret,
+                  generic_wid_hdl=l2cap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('L2CAP')
