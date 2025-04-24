81edbb72590ef2b11d9f531cf3d2451d9a771718
Lyle Zhu <lyle.zhu@nxp.com>
2025-04-24 12:20:49 +0800

    Zephyr: Support case L2CAP/FOC/BV-08-C
    Set IO capability to `no_input_output`.
    
    Start a L2CAP server with the PSM=0x1001 and enhance retransmission
    mode.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/l2cap.py
+++ autopts/ptsprojects/zephyr/l2cap.py
@@ -432,6 +432,9 @@ def test_cases(ptses):
         ZTestCase("L2CAP", "L2CAP/FOC/BV-06-C",
                   br_l2cap_success_eret,
                   generic_wid_hdl=l2cap_wid_hdl),
+        ZTestCase("L2CAP", "L2CAP/FOC/BV-08-C",
+                  br_l2cap_success_eret,
+                  generic_wid_hdl=l2cap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('L2CAP')
