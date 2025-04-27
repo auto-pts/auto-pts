b17efda86bb87e0ba1e89ce8d00db293518dff88
Lyle Zhu <lyle.zhu@nxp.com>
2025-04-27 18:07:42 +0800

    Zephyr: Support case L2CAP/CMC/BI-05-C
    Set IO capability to `no_input_output`.
    
    Start a L2CAP server with the PSM=0x1001 and basic mode.
    
    Update wids 14 and 22 to support Classic transport.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/l2cap.py
+++ autopts/ptsprojects/zephyr/l2cap.py
@@ -560,6 +560,9 @@ def test_cases(ptses):
         ZTestCase("L2CAP", "L2CAP/CMC/BV-09-C",
                   br_l2cap_success,
                   generic_wid_hdl=l2cap_wid_hdl),
+        ZTestCase("L2CAP", "L2CAP/CMC/BI-05-C",
+                  br_l2cap_success,
+                  generic_wid_hdl=l2cap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('L2CAP')
