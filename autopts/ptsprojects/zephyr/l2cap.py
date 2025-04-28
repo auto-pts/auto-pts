65128edb203b6606c0d3f8629f269b173cae69fa
Lyle Zhu <lyle.zhu@nxp.com>
2025-04-28 09:45:18 +0800

    Zephyr: Support case L2CAP/ECF/BV-01-C
    Set IO capability to `no_input_output`.
    
    Start a L2CAP server with the PSM=0x1001 and enhance retransmission
    mode (extended control).
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/l2cap.py
+++ autopts/ptsprojects/zephyr/l2cap.py
@@ -610,6 +610,9 @@ def test_cases(ptses):
         ZTestCase("L2CAP", "L2CAP/EWC/BV-03-C",
                   br_l2cap_success_eret_extended_control,
                   generic_wid_hdl=l2cap_wid_hdl),
+        ZTestCase("L2CAP", "L2CAP/ECF/BV-01-C",
+                  br_l2cap_success_eret_extended_control,
+                  generic_wid_hdl=l2cap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('L2CAP')
