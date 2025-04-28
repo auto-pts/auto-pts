f451d6839f52b127f4441a2001bd20cffa87f138
Lyle Zhu <lyle.zhu@nxp.com>
2025-04-28 14:31:02 +0800

    Zephyr: Support case L2CAP/LE/CID/BV-01-C
    Set IO capability to `no_input_output`.
    
    Start a LE L2CAP server with the PSM=0x80 and basic mode.
    
    Add wid 263 to create Classic L2CAP channel connect REQ.
    
    Update wid 23.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/ptsprojects/zephyr/l2cap.py
+++ autopts/ptsprojects/zephyr/l2cap.py
@@ -648,6 +648,9 @@ def test_cases(ptses):
         ZTestCase("L2CAP", "L2CAP/COS/CED/BV-11-C",
                   br_l2cap_success,
                   generic_wid_hdl=l2cap_wid_hdl),
+        ZTestCase("L2CAP", "L2CAP/LE/CID/BV-01-C",
+                  br_pre_cond + [TestFunc(stack.l2cap_init, le_psm, le_initial_mtu)],
+                  generic_wid_hdl=l2cap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('L2CAP')
