ecb434fc8cb37da92d02ba13498c363d1783c8c8
Lyle Zhu <lyle.zhu@nxp.com>
2025-05-26 10:08:01 +0800

    Zephyr: Support case L2CAP/CMC/BV-10-C
    Set IO capability to `no_input_output`.
    
    Start a L2CAP server with the PSM=0x1001 and stream mode (Mode
    optional).
    
    Update wid 119 to create L2CAP channel connection with optional stream
    mode.
    
    Update wids 14 and 22 to support Classic transport.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/wid/l2cap.py
+++ autopts/wid/l2cap.py
@@ -43,7 +43,8 @@ def hdl_wid_14(params: WIDParams):
                                  'L2CAP/COS/CFD/BV-13-C', 'L2CAP/ERM/BV-11-C',
                                  'L2CAP/ERM/BV-12-C', 'L2CAP/CMC/BI-01-C',
                                  'L2CAP/CMC/BI-02-C', 'L2CAP/CMC/BI-03-C',
-                                 'L2CAP/CMC/BI-04-C', 'L2CAP/CMC/BV-10-C']:
+                                 'L2CAP/CMC/BI-04-C', 'L2CAP/CMC/BV-10-C',
+                                 'L2CAP/CMC/BV-11-C']:
         l2cap = get_stack().l2cap
         for channel in l2cap.channels:
             _l2cap_chan_disconn_safely(channel.id)
@@ -91,7 +92,7 @@ def hdl_wid_22(params: WIDParams):
                                  'L2CAP/ERM/BV-11-C', 'L2CAP/ERM/BV-12-C',
                                  'L2CAP/CMC/BI-01-C', 'L2CAP/CMC/BI-02-C',
                                  'L2CAP/CMC/BI-03-C', 'L2CAP/CMC/BI-04-C',
-                                 'L2CAP/CMC/BV-10-C']:
+                                 'L2CAP/CMC/BV-10-C', 'L2CAP/CMC/BV-11-C']:
         btp.gap_disconn(bd_addr_type=defs.BTP_BR_ADDRESS_TYPE)
         return True
 
@@ -970,13 +971,18 @@ def hdl_wid_115(_: WIDParams):
     return True
 
 
-def hdl_wid_119(_: WIDParams):
+def hdl_wid_119(params: WIDParams):
     '''
     Please send L2CAP Information Request with InfoType to Extended Features(0x0002).
     '''
     btp.gap_wait_for_connection()
     l2cap = get_stack().l2cap
-    btp.l2cap_conn_v2(None, defs.BTP_BR_ADDRESS_TYPE, l2cap.psm, l2cap.initial_mtu,
-                      mode=defs.L2CAP_CONNECT_V2_MODE_ERET,
-                      options=defs.L2CAP_CONNECT_V2_OPT_MODE_OPTIONAL)
+    if params.test_case_name in ['L2CAP/CMC/BV-11-C']:
+        btp.l2cap_conn_v2(None, defs.BTP_BR_ADDRESS_TYPE, l2cap.psm, l2cap.initial_mtu,
+                          mode=defs.L2CAP_CONNECT_V2_MODE_STREAM,
+                          options=defs.L2CAP_CONNECT_V2_OPT_MODE_OPTIONAL)
+    else:
+        btp.l2cap_conn_v2(None, defs.BTP_BR_ADDRESS_TYPE, l2cap.psm, l2cap.initial_mtu,
+                          mode=defs.L2CAP_CONNECT_V2_MODE_ERET,
+                          options=defs.L2CAP_CONNECT_V2_OPT_MODE_OPTIONAL)
     return True
