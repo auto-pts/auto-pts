cf8656682be78a774d3951012f45893c8b265239
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-11-18 16:15:50 +0100

    btp: Add new ASCS events for CIS connection state
    Added 2 new events to indicate the state of the CIS
    associated with an ASE, as that cannot be determined directly
    from the ASE state.
    
    The "connected" event is the only one used so far, and
    is used to wait for the even when we expect the CIS to be
    connected.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- autopts/ptsprojects/stack/layers/ascs.py
+++ autopts/ptsprojects/stack/layers/ascs.py
@@ -23,6 +23,8 @@ class ASCS:
             defs.BTP_ASCS_EV_OPERATION_COMPLETED: [],
             defs.BTP_ASCS_EV_CHARACTERISTIC_SUBSCRIBED: [],
             defs.BTP_ASCS_EV_ASE_STATE_CHANGED: [],
+            defs.BTP_ASCS_EV_CIS_CONNECTED: [],
+            defs.BTP_ASCS_EV_CIS_DISCONNECTED: [],
         }
 
     def event_received(self, event_type, event_data_tuple):
@@ -46,3 +48,31 @@ class ASCS:
             lambda _addr_type, _addr, _ase_id, _state, *_:
             (addr_type, addr, ase_id, state) == (_addr_type, _addr, _ase_id, _state),
             timeout, remove)
+
+    def wait_ascs_cis_connected_ev(self, addr_type, addr, ase_id, cis_id, timeout=10, remove=True):
+        return wait_event_with_condition(
+            self.event_queues[defs.BTP_ASCS_EV_CIS_CONNECTED],
+            lambda _addr_type, _addr, _ase_id, _cis_id, *_: (addr_type, addr, ase_id, cis_id)
+            == (_addr_type, _addr, _ase_id, _cis_id),
+            timeout,
+            remove,
+        )
+
+    def wait_ascs_cis_disconnected_ev(
+        self, addr_type, addr, ase_id, cis_id, reason=None, timeout=10, remove=True
+    ):
+        def _matches(ev_addr_type, ev_addr, ev_ase_id, ev_cis_id, ev_reason, *_):
+            return (
+                ev_addr_type == addr_type
+                and ev_addr == addr
+                and ev_ase_id == ase_id
+                and ev_cis_id == cis_id
+                and (reason is None or ev_reason == reason)
+            )
+
+        return wait_event_with_condition(
+            self.event_queues[defs.BTP_ASCS_EV_CIS_DISCONNECTED],
+            _matches,
+            timeout,
+            remove,
+        )
