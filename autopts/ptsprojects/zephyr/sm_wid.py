d347a149d8031edc7f5e85008d19a971b646c038
alperen sener <alperen.sener@nordicsemi.no>
2025-11-20 10:40:30 +0100

    zephyr: sm: add wid 175, update wid 108
    WID 175 is added for test case SM/CEN/JW/BI-06-C
    Please confirm that IUT reports the Confirm Value Failed to the
    Upper Tester.
    
    Due to limitation on zephyr the same WID is added for zephyr
    because SMP failure is converted in zephyr.
    
    SM/CEN/JW/BV-01-C needs bondable flag to be disabled.

--- autopts/ptsprojects/zephyr/sm_wid.py
+++ autopts/ptsprojects/zephyr/sm_wid.py
@@ -15,6 +15,7 @@
 
 import logging
 
+from autopts.ptsprojects.stack import get_stack
 from autopts.ptsprojects.zephyr.iutctl import get_iut
 from autopts.pybtp import btp
 from autopts.wid import generic_wid_hdl
@@ -28,7 +29,8 @@ def sm_wid_hdl(wid, description, test_case_name):
 
 
 # wid handlers section begin
-def hdl_wid_143(desc):
+def hdl_wid_143(_):
+    """Confirm IUT readiness and read controller information."""
     zephyrctl = get_iut()
 
     zephyrctl.wait_iut_ready_event()
@@ -36,3 +38,19 @@ def hdl_wid_143(desc):
     btp.gap_read_ctrl_info()
 
     return True
+
+
+def hdl_wid_175(_):
+    """
+    Please confirm that IUT reports the Confirm Value Failed to the Upper Tester
+    """
+
+    stack = get_stack()
+    _, _, reason = stack.gap.gap_wait_for_pairing_fail(10)
+
+    # BT_SMP_ERR_CONFIRM_FAILED(0x04) is converted to BT_SECURITY_ERR_AUTH_FAIL(0x01)
+    # in zephyr host.
+    if reason == 0x01:
+        return True
+
+    return False
