05db9db8eae7ae7135bd3078d290898cd9dacc89
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-12-02 17:34:51 +0100

    CAP: Fix issues with handover tests
    Fix various issue with running the CAP handover tests.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- autopts/pybtp/types.py
+++ autopts/pybtp/types.py
@@ -16,7 +16,7 @@
 import struct
 from binascii import unhexlify
 from enum import IntEnum, IntFlag
-from typing import NamedTuple
+from typing import Final, NamedTuple
 
 from autopts.pybtp import defs
 
@@ -524,6 +524,10 @@ class BASSPASyncState:
     SYNCED = 0x02
     FAILED = 0x03
     NO_PAST = 0x04
+
+
+# 0xFFFF indicates "unknown" as per the BASS specification
+BASS_PA_INTERVAL_UNKNOWN: Final[int] = 0xFFFF
 
 
 class TMAPRole(IntFlag):
