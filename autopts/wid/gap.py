def4960e137d2cf6ce57ef9c3d35aa215eec622e
Lyle Zhu <lyle.zhu@nxp.com>
2026-07-22 18:54:21 +0800

    gap: add WID 249 handler for dedicated bonding procedure
    Implement handler for WID 249 which initiates bonding procedure in
    bondable mode with Dedicated Bonding specified in the Authentication
    Requirements.
    
    The handler enables bondable mode and starts the bonding procedure
    using a BR/EDR address with pairing level 2, fulfilling the test case
    requirement for Dedicated Bonding in bondable mode.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- autopts/wid/gap.py
+++ autopts/wid/gap.py
@@ -2915,3 +2915,13 @@ def hdl_wid_357(_: WIDParams):
         return False
 
     return True
+
+
+def hdl_wid_249(_: WIDParams):
+    '''
+    Please initiate the bonding procedure in bondable mode with Dedicated Bonding specified in the
+    Authentication_Requirements.
+    '''
+    btp.gap_set_bondable_on()
+    btp.gap_pair_v2(bd_addr_type=defs.BTP_BR_ADDRESS_TYPE, level=defs.BTP_GAP_CMD_PAIR_V2_LEVEL_2)
+    return True
