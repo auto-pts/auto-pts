c862ded885f84e2754549ea17ca60cdac1dced9f
Piotr Narajowski <piotr.narajowski@codecoup.pl>
2026-08-04 16:47:36 +0200

    mynewt: remove setting IXIT to default values
    Remove setting IXIT to default values in preconditions

--- autopts/ptsprojects/mynewt/gatt.py
+++ autopts/ptsprojects/mynewt/gatt.py
@@ -117,19 +117,7 @@ def set_pixits(ptses):
     pts = ptses[0]
 
     pts.set_pixit("GATT", "TSPX_bd_addr_iut", "DEADBEEFDEAD")
-    pts.set_pixit("GATT", "TSPX_iut_device_name_in_adv_packet_for_random_address", "")
-    pts.set_pixit("GATT", "TSPX_security_enabled", "FALSE")
-    pts.set_pixit("GATT", "TSPX_delete_link_key", "TRUE")
-    pts.set_pixit("GATT", "TSPX_time_guard", "180000")
-    pts.set_pixit("GATT", "TSPX_use_implicit_send", "TRUE")
-    pts.set_pixit("GATT", "TSPX_secure_simple_pairing_pass_key_confirmation", "FALSE")
-    pts.set_pixit("GATT", "TSPX_iut_is_client_periphral", "FALSE")
-    pts.set_pixit("GATT", "TSPX_iut_is_server_central", "FALSE")
-    pts.set_pixit("GATT", "TSPX_mtu_size", "23")
-    pts.set_pixit("GATT", "TSPX_pin_code", "0000")
-    pts.set_pixit("GATT", "TSPX_use_dynamic_pin", "FALSE")
     pts.set_pixit("GATT", "TSPX_delete_ltk", "TRUE")
-    pts.set_pixit("GATT", "TSPX_tester_appearance", "0000")
     pts.set_pixit("GATT", "TSPX_bearer_for_le", "EATT")
 
 
