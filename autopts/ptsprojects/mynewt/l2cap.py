bbaca2b4c9a1bdb44a7355ea4fff637e7d6c43e4
Piotr Narajowski <piotr.narajowski@codecoup.pl>
2026-09-14 12:26:57 +0200

    mynewt: l2cap: update pixit for L2CAP/ECFC/BV-22-C
    Set TSPX_iut_role_initiator to True. The test description itself states
    that IUT should be in initiator role. In L2CAP TS, the procedure says:
    Upper Tester commands the IUT to increase the MTU, and then the IUT
    sends L2CAP_CREDIT_BASED_RECONFIGURE_REQ. That makes the IUT the
    initiator of the reconfiguration procedure.

--- autopts/ptsprojects/mynewt/l2cap.py
+++ autopts/ptsprojects/mynewt/l2cap.py
@@ -182,6 +182,10 @@ def test_cases(ptses):
         ZTestCase("L2CAP", "L2CAP/TIM/BV-03-C",
                   pre_conditions_gatt,
                   generic_wid_hdl=l2cap_wid_hdl),
+        ZTestCase("L2CAP", "L2CAP/ECFC/BV-22-C",
+                  pre_conditions +
+                  [TestFunc(lambda: pts.update_pixit_param("L2CAP", "TSPX_iut_role_initiator", "True"))],
+                  generic_wid_hdl=l2cap_wid_hdl),
     ]
 
     test_case_name_list = pts.get_test_case_list('L2CAP')
